c2aa3031824e0340c76244314998acb66819173d
Matthieu Ancellin <matthieu.ancellin@ucd.ie>
2017-09-01 19:34:32 +0100

    WIP: conda packaging.

--- setup.py
+++ setup.py
@@ -1,14 +1,40 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from setuptools import setup
+from numpy.distutils.core import Extension, setup
 
-setup(name='capytaine',
-      version='0.1',
-      description='Nemoh python wrapper',
-      url='http://github.com/mancellin/capytaine',
-      author='Matthieu Ancellin',
-      author_email='matthieu.ancellin@ucd.ie',
-      license='GPLv3',
-      packages=['capytaine'],
-      zip_safe=False)
+Green = Extension(
+    name="capytaine._Green",
+    sources=[
+        "capytaine/NemohCore/Green_1.f90",
+        "capytaine/NemohCore/Initialize_Green_2.f90",
+        "capytaine/NemohCore/Green_2.f90",
+        "capytaine/NemohCore/Build_matrices.f90",
+    ]
+)
+
+Wavenumber = Extension(
+    name="capytaine._Wavenumber",
+    sources=[
+        "capytaine/NemohCore/Wavenumber.f90"
+    ]
+)
+
+if __name__ == "__main__":
+    setup(name='capytaine',
+          version='0.1',
+          description='Nemoh python wrapper',
+          # url='http://github.com/mancellin/capytaine',
+          author='Matthieu Ancellin',
+          author_email='matthieu.ancellin@ucd.ie',
+          license='GPLv3',
+          packages=['capytaine'],
+          install_requires=[
+              'numpy',
+              'meshmagick',
+              ],
+          ext_modules=[
+              Green,
+              Wavenumber
+          ],
+         )
