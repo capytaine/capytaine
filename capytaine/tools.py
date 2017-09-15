817c83fd673032e995fa81976d41e3e682b60be2
Matthieu Ancellin <matthieu.ancellin@ucd.ie>
2017-09-15 08:44:45 +0100

    Add MaxLengthDict class.

---
+++ capytaine/tools.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from collections import OrderedDict
+
+class MaxLengthDict(OrderedDict):
+    """Dictionary with limited number of entries. When maximum size is
+    reached, the oldest entry is dropped at the insertion of a new one.
+    """
+    def __init__(self, *args, max_length=1, **kwargs):
+        assert isinstance(max_length, int)
+        assert max_length > 0
+        self.__max_length__ = max_length
+        OrderedDict.__init__(self, *args, **kwargs)
+
+    def __setitem__(self, key, val):
+        if key in self:
+            del self[key]
+        OrderedDict.__setitem__(self, key, val)
+        if len(self) > self.__max_length__:
+            self.popitem(last=False)
---
+++ pytest/test_tools.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from capytaine.tools import MaxLengthDict
+
+def test_MaxLengthDict():
+    dc = MaxLengthDict({'a':1, 'b':5, 'c':3}, max_length=4)
+    assert dc.__max_length__ == 4
+    assert list(dc.keys()) == ['a', 'b', 'c']
+    dc['d'] = 8
+    assert list(dc.keys()) == ['a', 'b', 'c', 'd']
+    dc['e'] = 2 # drop 'a'
+    assert list(dc.keys()) == ['b', 'c', 'd', 'e']
+    dc['b'] = 7 # move 'b' to front
+    assert list(dc.keys()) == ['c', 'd', 'e', 'b']
+    dc['f'] = 4 # drop 'c'
+    assert list(dc.keys()) == ['d', 'e', 'b', 'f']
+    dc.update({'g':6, 'h':9})
+    assert list(dc.keys()) == ['b', 'f', 'g', 'h']
+
+    dc2 = MaxLengthDict({'a':1, 'b':5, 'c':3}, max_length=1)
+    assert dc2.__max_length__ == 1
+    assert list(dc2.keys()) == ['c']
