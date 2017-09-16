54e813b4e2f1a1354a575617c02d5d3c5b5c4a05
Matthieu Ancellin <matthieu.ancellin@ucd.ie>
2017-09-16 17:03:31 +0100

    Remove unused symmetry code in Fortran.

--- capytaine/NemohCore/Green_2.f90
+++ capytaine/NemohCore/Green_2.f90
@@ -156,16 +156,10 @@ CONTAINS
     ! Local variables
     REAL                     :: ADPI, ADPI2, AKDPI, AKDPI2
     REAL, DIMENSION(3)       :: XI
-    COMPLEX, DIMENSION(1)    :: FS
-    COMPLEX, DIMENSION(3, 1) :: VS
 
     XI(:) = X0I(:)
     ! XI(3) = MIN(X0I(3), -1e-5*Mesh%xy_diameter)
-    CALL COMPUTE_S2(XI, X0J, 0.0, wavenumber, FS(1), VS(:, 1))
-
-    ! IF (Mesh%Isym == 0) THEN
-    SP       = FS(1)
-    VSP(1:3) = VS(1:3, 1)
+    CALL COMPUTE_S2(XI, X0J, 0.0, wavenumber, SP, VSP(:))
 
     ADPI2  = wavenumber*AJ/DPI2
     ADPI   = wavenumber*AJ/DPI
@@ -199,53 +193,52 @@ CONTAINS
     REAL                        :: AMH, AKH, A, COF1, COF2, COF3, COF4
     REAL                        :: AQT, RRR
     REAL, DIMENSION(3)          :: XI, XJ
-    REAL, DIMENSION(4, 1)       :: FTS, PSR
-    REAL, DIMENSION(3, 4, 1)    :: VTS
-    COMPLEX, DIMENSION(4, 1)    :: FS
-    COMPLEX, DIMENSION(3, 4, 1) :: VS
+    REAL, DIMENSION(4)          :: FTS, PSR
+    REAL, DIMENSION(3, 4)       :: VTS
+    COMPLEX, DIMENSION(4)       :: FS
+    COMPLEX, DIMENSION(3, 4)    :: VS
 
     !========================================
     ! Part 1: Solve 4 infinite depth problems
     !========================================
 
     XI(:) = X0I(:)
-    ! XI(3) = MIN(X0I(3), -EPS*Mesh%xy_diameter)
     XJ(:) = X0J(:)
 
     ! Distance in xOy plane
     RRR = NORM2(XI(1:2) - XJ(1:2))
 
     ! 1.a First infinite depth problem
-    CALL COMPUTE_S2(XI(:), XJ(:), depth, wavenumber, FS(1, 1), VS(:, 1, 1))
+    CALL COMPUTE_S2(XI(:), XJ(:), depth, wavenumber, FS(1), VS(:, 1))
 
-    PSR(1, 1) = PI/(wavenumber*SQRT(RRR**2+(XI(3)+XJ(3))**2))
+    PSR(1) = PI/(wavenumber*SQRT(RRR**2+(XI(3)+XJ(3))**2))
 
     ! 1.b Shift and reflect XI and compute another value of the Green function
     XI(3) = -X0I(3) - 2*depth
     XJ(3) =  X0J(3)
-    CALL COMPUTE_S2(XI(:), XJ(:), depth, wavenumber, FS(2, 1), VS(:, 2, 1))
-    VS(3, 2, 1) = -VS(3, 2, 1) ! Reflection of the output vector
+    CALL COMPUTE_S2(XI(:), XJ(:), depth, wavenumber, FS(2), VS(:, 2))
+    VS(3, 2) = -VS(3, 2) ! Reflection of the output vector
 
-    PSR(2, 1) = PI/(wavenumber*SQRT(RRR**2+(XI(3)+XJ(3))**2))
+    PSR(2) = PI/(wavenumber*SQRT(RRR**2+(XI(3)+XJ(3))**2))
 
     ! 1.c Shift and reflect XJ and compute another value of the Green function
     XI(3) =  X0I(3)
     XJ(3) = -X0J(3) - 2*depth
-    CALL COMPUTE_S2(XI(:), XJ(:), depth, wavenumber, FS(3, 1), VS(:, 3, 1))
+    CALL COMPUTE_S2(XI(:), XJ(:), depth, wavenumber, FS(3), VS(:, 3))
 
-    PSR(3, 1) = PI/(wavenumber*SQRT(RRR**2+(XI(3)+XJ(3))**2))
+    PSR(3) = PI/(wavenumber*SQRT(RRR**2+(XI(3)+XJ(3))**2))
 
     ! 1.d Shift and reflect both XI and XJ and compute another value of the Green function
     XI(3) = -X0I(3) - 2*depth
     XJ(3) = -X0J(3) - 2*depth
-    CALL COMPUTE_S2(XI(:), XJ(:), depth, wavenumber, FS(4, 1), VS(:, 4, 1))
-    VS(3, 4, 1) = -VS(3, 4, 1) ! Reflection of the output vector
+    CALL COMPUTE_S2(XI(:), XJ(:), depth, wavenumber, FS(4), VS(:, 4))
+    VS(3, 4) = -VS(3, 4) ! Reflection of the output vector
 
-    PSR(4, 1) = PI/(wavenumber*SQRT(RRR**2+(XI(3)+XJ(3))**2))
+    PSR(4) = PI/(wavenumber*SQRT(RRR**2+(XI(3)+XJ(3))**2))
 
     ! Add up the results of the four problems
-    SP       = -SUM(FS(1:4, 1)) - SUM(PSR(1:4, 1))
-    VSP(1:3) = -SUM(VS(1:3, 1:4, 1), 2)
+    SP       = -SUM(FS(1:4)) - SUM(PSR(1:4))
+    VSP(1:3) = -SUM(VS(1:3, 1:4), 2)
 
     ! Multiply by some coefficients
     AMH  = wavenumber*depth
@@ -271,27 +264,27 @@ CONTAINS
 
       ! 2.a Shift observation point and compute integral
       XI(3) =  X0I(3) + depth*AMBDA(KE) - 2*depth
-      CALL COMPUTE_ASYMPTOTIC_S0(XI(:), X0J(:), AJ, FTS(1, 1), VTS(:, 1, 1))
+      CALL COMPUTE_ASYMPTOTIC_S0(XI(:), X0J(:), AJ, FTS(1), VTS(:, 1))
 
       ! 2.b Shift and reflect observation point and compute integral
       XI(3) = -X0I(3) - depth*AMBDA(KE)
-      CALL COMPUTE_ASYMPTOTIC_S0(XI(:), X0J(:), AJ, FTS(2, 1), VTS(:, 2, 1))
-      VTS(3, 2, 1) = -VTS(3, 2, 1) ! Reflection of the output vector
+      CALL COMPUTE_ASYMPTOTIC_S0(XI(:), X0J(:), AJ, FTS(2), VTS(:, 2))
+      VTS(3, 2) = -VTS(3, 2) ! Reflection of the output vector
 
       ! 2.c Shift and reflect observation point and compute integral
       XI(3) = -X0I(3) + depth*AMBDA(KE) - 4*depth
-      CALL COMPUTE_ASYMPTOTIC_S0(XI(:), X0J(:), AJ, FTS(3, 1), VTS(:, 3, 1))
-      VTS(3, 3, 1) = -VTS(3, 3, 1) ! Reflection of the output vector
+      CALL COMPUTE_ASYMPTOTIC_S0(XI(:), X0J(:), AJ, FTS(3), VTS(:, 3))
+      VTS(3, 3) = -VTS(3, 3) ! Reflection of the output vector
 
       ! 2.d Shift observation point and compute integral
       XI(3) =  X0I(3) - depth*AMBDA(KE) + 2*depth
-      CALL COMPUTE_ASYMPTOTIC_S0(XI(:), X0J(:), AJ, FTS(4, 1), VTS(:, 4, 1))
+      CALL COMPUTE_ASYMPTOTIC_S0(XI(:), X0J(:), AJ, FTS(4), VTS(:, 4))
 
       AQT = -AR(KE)/(8*PI)
 
       ! Add all the contributions
-      SP     = SP     + AQT*SUM(FTS(1:4, 1))
-      VSP(:) = VSP(:) + AQT*SUM(VTS(1:3, 1:4, 1), 2)
+      SP       = SP       + AQT*SUM(FTS(1:4))
+      VSP(1:3) = VSP(1:3) + AQT*SUM(VTS(1:3, 1:4), 2)
 
     END DO
 
