0f3e924a4a1386ea7e48349b8353b5db718a2539
Matthieu Ancellin <matthieu.ancellin@ucd.ie>
2017-10-20 18:02:14 +0100

    WIP: diffraction problem.

--- capytaine/problems.py
+++ capytaine/problems.py
@@ -9,10 +9,8 @@ from warnings import warn
 import numpy as np
 
 from capytaine._Wavenumber import invert_xtanhx
-from capytaine.symmetries import ReflectionSymmetry
 
-class RadiationProblem:
-    """A radiation problem to be solved by the BEM solver."""
+class PotentialFlowProblem:
 
     def __init__(self, body, free_surface=0.0, sea_bottom=-np.infty, omega=1.0, rho=1000.0, g=9.81):
         self.rho = rho
@@ -34,6 +32,49 @@ class RadiationProblem:
             warn(f"""The mesh of the body {body.name} is not inside the domain.\nUse body.get_immersed_part() to clip the mesh.""")
         self.body = body
 
+    @property
+    def depth(self):
+        return self.free_surface - self.sea_bottom
+
+
+class DiffractionProblem(PotentialFlowProblem):
+
+    def __init__(self, *args, angle=0.0, **kwargs):
+        self.angle = angle
+        PotentialFlowProblem.__init__(self, *args, **kwargs)
+
+    def __str__(self):
+        return f"Diffraction problem of {self.body.name} with depth={self.free_surface-self.sea_bottom:.1e}, angle={self.angle:.3f} and omega={self.omega:.3f}"
+
+    def __repr__(self):
+        return f"DiffractionProblem(body={self.body.name}, free_surface={self.free_surface}, sea_bottom={self.sea_bottom}, angle={self.angle}, omega={self.omega}, rho={self.rho}, g={self.g})"
+
+    def airy_wave(self, X):
+
+        x, y, z = X
+
+        XEFF, YEFF = 0, 0
+        wbar = (x-XEFF)*np.cos(self.angle) + (y-YEFF)*np.sin(self.angle)
+
+        if self.wavenumber*self.depth < 20 and self.wavenumber*self.depth >= 0:
+            cih = np.cosh(self.wavenumber*(z+self.depth))/np.cosh(self.wavenumber*self.depth)
+            sih = np.sinh(self.wavenumber*(z+self.depth))/np.sinh(self.wavenumber*self.depth)
+        else:
+            cih = np.exp(self.wavenumber*z)
+            sih = np.exp(self.wavenumber*z)
+
+        v = self.g*self.wavenumber/self.omega * \
+                np.array([np.cos(self.angle)*cih, np.sin(self.angle)*cih, -1j*sih]) * \
+                np.exp(1j * self.wavenumber * wbar)
+
+        p = self.rho * self.g * cih * np.exp(1j * self.wavenumber * wbar)
+
+        return p, v
+
+
+class RadiationProblem(PotentialFlowProblem):
+    """A radiation problem to be solved by the BEM solver."""
+
     def __str__(self):
         return f"Radiation problem of {self.body.name} with depth={self.free_surface-self.sea_bottom:.1e} and omega={self.omega:.3f}"
 
@@ -41,86 +82,6 @@ class RadiationProblem:
         return f"RadiationProblem(body={self.body.name}, free_surface={self.free_surface}, sea_bottom={self.sea_bottom}, omega={self.omega}, rho={self.rho}, g={self.g})"
 
     @property
-    def depth(self):
-        return self.free_surface - self.sea_bottom
+    def dofs(self):
+        return self.body.dofs
 
-    def export_as_Nemoh_directory(self, directory_name, omega_range=None):
-        import os
-        from meshmagick.mmio import write_MAR
-
-        if os.path.isdir(directory_name):
-            warn(f"Exporting problem in existing directory: {directory_name}")
-        else:
-            os.makedirs(directory_name)
-
-        write_MAR(
-            os.path.join(directory_name, f'{self.body.name}.dat'),
-            self.body.vertices,
-            self.body.faces,
-            xOz_symmetry=isinstance(self.body, ReflectionSymmetry)
-        )
-
-
-        if omega_range is None:
-            omega_nb_steps = 1
-            omega_start = omega
-            omega_stop = omega
-        else:
-            omega_nb_steps = len(omega_range)
-            omega_start = min(omega_range)
-            omega_stop = max(omega_range)
-
-        with open(os.path.join(directory_name, "Nemoh.cal"), "w") as nemoh_cal:
-            nemoh_cal.write(
-                    DEFAULT_NEMOH_CAL.format(
-                        rho=self.rho,
-                        g=self.g,
-                        depth=self.depth,
-                        mesh_filename=f'{self.body.name}.dat',
-                        mesh_vertices=self.body.nb_vertices,
-                        mesh_faces=self.body.nb_faces,
-                        omega_nb_steps=omega_nb_steps,
-                        omega_start=omega_start,
-                        omega_stop=omega_stop,
-                        )
-                    )
-
-        with open(os.path.join(directory_name, "input.txt"), "w") as input_txt:
-            input_txt.write(DEFAULT_INPUT_TXT)
-
-        with open(os.path.join(directory_name, "ID.dat"), "w") as id_dat:
-            id_dat.write(f"1\n.")
-
-
-DEFAULT_NEMOH_CAL = """--- Environment ------------------------------------------------------------------------------------------------------------------
-{rho}			! RHO 			! KG/M**3 	! Fluid specific volume
-{g}				! G			! M/S**2	! Gravity
-{depth}			! DEPTH			! M		! Water depth
-0.	0.			! XEFF YEFF		! M		! Wave measurement point
---- Description of floating bodies -----------------------------------------------------------------------------------------------
-1				! Number of bodies
---- Body 1 -----------------------------------------------------------------------------------------------------------------------
-{mesh_filename}
-{mesh_vertices} {mesh_faces}
-1				! Number of degrees of freedom
-1 0. 0. 1. 0. 0. 0.		! Heave
-1				! Number of resulting generalised forces
-1 0. 0. 1. 0. 0. 0.		! Heave
-0				! Number of lines of additional information
---- Load cases to be solved -------------------------------------------------------------------------------------------------------
-{omega_nb_steps} {omega_start} {omega_stop} ! Frequencies range
-0	0.	0.		! Number of wave directions, Min and Max (degrees)
---- Post processing ---------------------------------------------------------------------------------------------------------------
-0	0.1	10.		! IRF 				! IRF calculation (0 for no calculation), time step and duration
-0				! Show pressure
-0 	0.	180.		! Kochin function 		! Number of directions of calculation (0 for no calculations), Min and Max (degrees)
-0	0	100.	100.	! Free surface elevation 	! Number of points in x direction (0 for no calcutions) and y direction and dimensions of domain in x and y direction
-"""
-
-DEFAULT_INPUT_TXT="""--- Calculation parameters ------------------------------------------------------------------------------------
-1				! Indiq_solver		! - 		! Solver (0) Direct Gauss (1) GMRES (2) GMRES with FMM acceleration (2 not implemented yet)
-20				! IRES			! - 		! Restart parameter for GMRES
-5.E-07				! TOL_GMRES		! -		! Stopping criterion for GMRES
-100				! MAXIT			! - 		! Maximum iterations for GMRES
-1				! Sav_potential		! -		! Save potential for visualization
-"""
--- pytest/test_problems.py
+++ pytest/test_problems.py
@@ -5,13 +5,20 @@ import pytest
 
 import numpy as np
 
-from capytaine.problems import RadiationProblem
+from capytaine.problems import *
 from capytaine.reference_bodies import DummyBody
 
+dummy = DummyBody()
+
 def test_depth():
-    dummy = DummyBody()
-    assert RadiationProblem(dummy, free_surface=np.infty, sea_bottom=-np.infty).depth == np.infty
-    assert RadiationProblem(dummy, free_surface=0.0, sea_bottom=-np.infty).depth == np.infty
-    assert RadiationProblem(dummy, free_surface=0.0, sea_bottom=-1.0).depth == 1.0
+    assert PotentialFlowProblem(dummy, free_surface=np.infty, sea_bottom=-np.infty).depth == np.infty
+    assert PotentialFlowProblem(dummy, free_surface=0.0, sea_bottom=-np.infty).depth == np.infty
+    assert PotentialFlowProblem(dummy, free_surface=0.0, sea_bottom=-1.0).depth == 1.0
+
     with pytest.raises(Exception):
-        RadiationProblem(dummy, free_surface=0.0, sea_bottom=1.0)
+        PotentialFlowProblem(dummy, free_surface=0.0, sea_bottom=1.0)
+
+# def test_Airy():
+#     dp = DiffractionProblem(dummy, free_surface=0.0, sea_bottom=-np.infty, omega=1.0)
+#     print(dp.airy_wave((0.0, 0.0, -5.0)))
+
