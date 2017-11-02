bcc6df5b3e44243f63a2a9603f01bc35439baad8
Matthieu Ancellin <matthieu.ancellin@ucd.ie>
2017-11-02 16:53:50 +0000

    Fix cylinder benchmark.

--- benchmark/cylinder.py
+++ benchmark/cylinder.py
@@ -7,7 +7,7 @@ from itertools import count, product
 import numpy as np
 import pandas as pd
 
-from capytaine.reference_bodies import HorizontalCylinder
+from capytaine.reference_bodies import generate_horizontal_cylinder, generate_clever_horizontal_cylinder
 from capytaine.symmetries import *
 
 from benchmark import *
@@ -17,20 +17,20 @@ WORKING_DIRECTORY = datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S")
 ID = count()
 
 def full_resolution_Nemoh(nb_slices, nb_theta, omega_range):
-    cylinder = HorizontalCylinder(length=10.0, radius=1.0, nx=nb_slices+1, nr=0, ntheta=nb_theta+1)
+    cylinder = generate_horizontal_cylinder(length=10.0, radius=1.0, nx=nb_slices+1, nr=0, ntheta=nb_theta+1)
     cylinder.translate_x(-5.0)
     cylinder.translate_z(-2.0)
     return profile_Nemoh(cylinder, omega_range, f"{WORKING_DIRECTORY}/{next(ID):03}_Nemoh_{nb_theta*nb_slices}")
 
 def full_Capytaine(nb_slices, nb_theta, omega_range):
-    cylinder = HorizontalCylinder(length=10.0, radius=1.0, nx=nb_slices+1, nr=0, ntheta=nb_theta+1)
+    cylinder = generate_horizontal_cylinder(length=10.0, radius=1.0, nx=nb_slices+1, nr=0, ntheta=nb_theta+1)
     cylinder.translate_x(-5.0)
     cylinder.translate_z(-2.0)
     cylinder.dofs["Heave"] = cylinder.faces_normals @ (0, 0, 1)
     return profile_capytaine(cylinder, omega_range, f"{WORKING_DIRECTORY}/{next(ID):03}_capy_{nb_theta*nb_slices}")
 
 def sym_Capytaine(nb_slices, nb_theta, omega_range):
-    cylinder = HorizontalCylinder(length=10.0, radius=1.0, nx=nb_slices+1, nr=0, ntheta=nb_theta+1)
+    cylinder = generate_horizontal_cylinder(length=10.0, radius=1.0, nx=nb_slices+1, nr=0, ntheta=nb_theta+1)
     cylinder.translate_x(-5.0)
     cylinder.translate_z(-2.0)
     half_cylinder = cylinder.extract_faces(np.where(cylinder.faces_centers[:, 1] > 0)[0])
@@ -40,7 +40,7 @@ def sym_Capytaine(nb_slices, nb_theta, omega_range):
     return profile_capytaine(sym_cylinder, omega_range, f"{WORKING_DIRECTORY}/{next(ID):03}_sym_capy_{nb_theta*nb_slices}")
 
 def sym_sym_Capytaine(nb_slices, nb_theta, omega_range):
-    cylinder = HorizontalCylinder(length=10.0, radius=1.0, nx=nb_slices+1, nr=0, ntheta=nb_theta+1)
+    cylinder = generate_horizontal_cylinder(length=10.0, radius=1.0, nx=nb_slices+1, nr=0, ntheta=nb_theta+1)
     cylinder.translate_x(-5.0)
     cylinder.translate_z(-2.0)
     half_cylinder = cylinder.extract_faces(np.where(cylinder.faces_centers[:, 1] > 0)[0])
@@ -51,16 +51,14 @@ def sym_sym_Capytaine(nb_slices, nb_theta, omega_range):
     return profile_capytaine(sym_sym_cylinder, omega_range, f"{WORKING_DIRECTORY}/{next(ID):03}_sym_sym_capy_{nb_theta*nb_slices}")
 
 def trans_Capytaine(nb_slices, nb_theta, omega_range):
-    ring = HorizontalCylinder(length=10.0/nb_slices, radius=1.0, nx=2, nr=0, ntheta=nb_theta+1)
-    ring.translate_x(-5.0)
-    ring.translate_z(-2.0)
-    ring.name = "ring"
-    trans_cylinder = TranslationalSymmetry(ring, translation=np.asarray([10.0/nb_slices, 0.0, 0.0]), nb_repetitions=nb_slices-1)
+    trans_cylinder = generate_clever_horizontal_cylinder(length=10.0, radius=1.0, nx=nb_slices+1, ntheta=nb_theta+1)
+    trans_cylinder.translate_x(-5.0)
+    trans_cylinder.translate_z(-2.0)
     trans_cylinder.dofs["Heave"] = trans_cylinder.faces_normals @ (0, 0, 1)
     return profile_capytaine(trans_cylinder, omega_range, f"{WORKING_DIRECTORY}/{next(ID):03}_trans_capy_{nb_theta*nb_slices}")
 
 def trans_sym_Capytaine(nb_slices, nb_theta, omega_range):
-    ring = HorizontalCylinder(length=10.0/nb_slices, radius=1.0, nx=2, nr=0, ntheta=nb_theta+1)
+    ring = generate_horizontal_cylinder(length=10.0/nb_slices, radius=1.0, nx=2, nr=0, ntheta=nb_theta+1)
     half_ring = ring.extract_faces(np.where(ring.faces_centers[:, 1] > 0)[0])
     half_ring.translate_x(-5.0)
     half_ring.translate_z(-2.0)
@@ -70,7 +68,7 @@ def trans_sym_Capytaine(nb_slices, nb_theta, omega_range):
     return profile_capytaine(trans_cylinder, omega_range, f"{WORKING_DIRECTORY}/{next(ID):03}_trans_sym_capy_{nb_theta*nb_slices}")
 
 def sym_trans_Capytaine(nb_slices, nb_theta, omega_range):
-    ring = HorizontalCylinder(length=10.0/nb_slices, radius=1.0, nx=2, nr=0, ntheta=nb_theta+1)
+    ring = generate_horizontal_cylinder(length=10.0/nb_slices, radius=1.0, nx=2, nr=0, ntheta=nb_theta+1)
     half_ring = ring.extract_faces(np.where(ring.faces_centers[:, 1] > 0)[0])
     half_ring.translate_x(-5.0)
     half_ring.translate_z(-2.0)
