09a33f65151773bd84c396357031ddf0bc5c289c
Matthieu Ancellin <matthieu.ancellin@ucd.ie>
2017-11-03 14:20:45 +0000

    Improve post-processing of benchmarks.

--- benchmark/plot_csv.py
+++ benchmark/plot_csv.py
@@ -9,49 +9,95 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-directory = sys.argv[1]
-# directory = '2017-10-02_173227/'
-
 #######################################################################
 #                              Plot time                              #
 #######################################################################
+def compare_all_total_times(directory):
+    times = pd.DataFrame.from_csv(os.path.join(directory, 'times.csv'))
 
-times = pd.DataFrame.from_csv(os.path.join(directory, 'times.csv'))
+    # print(times.groupby('nb_cells').aggregate(np.std))
 
-print(times.groupby('nb_cells').aggregate(np.std))
+    times = times.groupby('nb_cells').aggregate(np.min)
+    ax = times.plot()
+    ax.set(
+        xlabel='number of cells in mesh',
+        ylabel='computation time (seconds)',
+    )
+    plt.grid()
 
-times = times.groupby('nb_cells').aggregate(np.min)
-ax = times.plot()
-ax.set(xlabel='number of cells in mesh', ylabel='computation time (seconds)')
 
-plt.grid()
+#######################################################################
+#                            Time details                             #
+#######################################################################
+def plot_detailed_time(directory):
+    dirs = glob.glob(os.path.join(directory, '*capy*'))
+    detailed_time = pd.DataFrame(
+        index=dirs,
+        columns=['type', 'nb_cells', 'evaluate matrices', 'solve linear problem', 'total'],
+    )
+
+    for result_dir in dirs:
+
+        detailed_time['nb_cells'][result_dir] = int(result_dir.split('_')[-1])
+        detailed_time['type'][result_dir] = '_'.join(result_dir.split('_')[2:-1])
+
+        with open(os.path.join(result_dir, 'profile.log'), 'r') as profile_file:
+            for entry in profile_file.readlines():
+                if '(build_matrices)' in entry:
+                    detailed_time['evaluate matrices'][result_dir] = float(entry.split()[3])
+                elif '(solve)' in entry and 'numpy/linalg' in entry:
+                    detailed_time['solve linear problem'][result_dir] = float(entry.split()[3])
+                elif '(solve)' in entry and 'Nemoh.py' in entry:
+                    detailed_time['total'][result_dir] = float(entry.split()[3])
+
+    detailed_time['other'] = detailed_time['total'] - detailed_time['evaluate matrices'] - detailed_time['solve linear problem']
+    detailed_time = detailed_time.sort_values(by='nb_cells')
+
+    for name, group in detailed_time.groupby('type'):
+        ax = group.plot.area(
+            x='nb_cells',
+            y=['solve linear problem', 'evaluate matrices', 'other'],
+        )
+        ax.set(
+            ylim=(0.0, 60.0),
+            title=name,
+            xlabel='number of cells in mesh',
+            ylabel='computation time (seconds)',
+        )
+        plt.grid(zorder=3)
+
 
 #######################################################################
 #                            Check values                             #
 #######################################################################
-omega_range = np.linspace(0.1, 4.0, 40)
+def compare_results(directory):
+    omega_range = np.linspace(0.1, 4.0, 40)
 
-nemoh_dirs = glob.glob(os.path.join(directory, '*Nemoh*'))
-capy_dirs = glob.glob(os.path.join(directory, '*capy*'))
+    nemoh_dirs = glob.glob(os.path.join(directory, '*Nemoh*'))
+    capy_dirs = glob.glob(os.path.join(directory, '*capy*'))
 
-case_names = sorted(os.path.basename(name) for name in nemoh_dirs+capy_dirs) 
+    case_names = sorted(os.path.basename(name) for name in nemoh_dirs+capy_dirs)
 
-added_mass = pd.DataFrame(index=omega_range, columns=case_names)
-damping = pd.DataFrame(index=omega_range, columns=case_names)
+    added_mass = pd.DataFrame(index=omega_range, columns=case_names)
+    damping = pd.DataFrame(index=omega_range, columns=case_names)
 
-for nemoh_dir in nemoh_dirs:
-    mesh = int(nemoh_dir.split('_')[-1])
-    results = np.genfromtxt(os.path.join(nemoh_dir, "results", "Forces.dat"))
-    added_mass[os.path.basename(nemoh_dir)] = results[::2]
-    damping[os.path.basename(nemoh_dir)] = results[1::2]
+    for nemoh_dir in nemoh_dirs:
+        mesh = int(nemoh_dir.split('_')[-1])
+        results = np.genfromtxt(os.path.join(nemoh_dir, "results", "Forces.dat"))
+        added_mass[os.path.basename(nemoh_dir)] = results[::2]
+        damping[os.path.basename(nemoh_dir)] = results[1::2]
 
-for capy_dir in capy_dirs:
-    mesh = int(capy_dir.split('_')[-1])
-    results = np.genfromtxt(os.path.join(capy_dir, "results.csv"))
-    added_mass[os.path.basename(capy_dir)] = results[:, 0]
-    damping[os.path.basename(capy_dir)] = results[:, 1]
+    for capy_dir in capy_dirs:
+        mesh = int(capy_dir.split('_')[-1])
+        results = np.genfromtxt(os.path.join(capy_dir, "results.csv"))
+        added_mass[os.path.basename(capy_dir)] = results[:, 0]
+        damping[os.path.basename(capy_dir)] = results[:, 1]
 
-# added_mass.plot(y=[name for name in case_names if '600' in name])
-# print(added_mass[[name for name in case_names if '600' in name]])
+    # added_mass.plot(y=[name for name in case_names if '600' in name])
+    # print(added_mass[[name for name in case_names if '600' in name]])
 
-plt.show()
+
+if __name__ == "__main__":
+    plot_detailed_time(sys.argv[1])
+    plt.show()
+
