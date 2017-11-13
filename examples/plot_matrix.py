bf37dd71c41b9de1ebdb763a4f21f0c9de7211bb
Matthieu Ancellin <matthieu.ancellin@ucd.ie>
2017-11-13 14:26:20 +0000

    Fix plot matrix example.

--- examples/plot_matrix.py
+++ examples/plot_matrix.py
@@ -10,22 +10,14 @@ from capytaine.reference_bodies import *
 from capytaine.symmetries import *
 from capytaine.Nemoh import Nemoh
 
-# cylinder = HorizontalCylinder(length=10.0, radius=1.0, nx=10, nr=0, ntheta=10)
-# cylinder.translate_z(-2.0)
+cylinder = generate_clever_horizontal_cylinder(length=10.0, radius=1.0, nx=5, ntheta=10)
+cylinder.translate_z(-2.0)
 # cylinder.show()
-
-nb_slices = 5
-nb_theta = 10
-ring = HorizontalCylinder(length=10.0/nb_slices, radius=1.0, nx=2, nr=0, ntheta=nb_theta+1)
-ring.translate_x(-5.0)
-ring.translate_z(-2.0)
-ring.name = "ring"
-cylinder = TranslationalSymmetry(ring, translation=np.asarray([10.0/nb_slices, 0.0, 0.0]), nb_repetitions=nb_slices-1)
-cylinder.show()
 
 solver = Nemoh()
 
-S, V = cylinder.build_matrices(cylinder)
+S, V = cylinder.build_matrices(cylinder, free_surface=np.infty)
+# S, V = cylinder.build_matrices(cylinder)
 
 plt.imshow(np.abs(V.full_matrix()))
 plt.colorbar()
