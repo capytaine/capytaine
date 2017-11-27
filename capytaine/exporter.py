58bda8df7242bbc7529a72fb8a795b3566b22237
Matthieu Ancellin <matthieu.ancellin@ucd.ie>
2017-11-27 16:35:11 +0000

    Comment exporter file.

--- capytaine/exporter.py
+++ capytaine/exporter.py
@@ -1,32 +1,43 @@
 #!/usr/bin/env python
 # coding: utf-8
-"""
-Export problems as Nemoh 2.0 directory.
-"""
 
 import os
-import numpy as np
 
 from meshmagick.mmio import write_MAR
 from capytaine.symmetries import ReflectionSymmetry
 
 def export_as_Nemoh_directory(problem, directory_name, omega_range=None):
     """
+    Export radiation problems as Nemoh 2.0 directory (experimental).
+
     TODO: Diffraction problem.
+
+    Parameters
+    ----------
+    problem: RadiationProblem
+        the problem that should be exported
+    directory_name: string
+        path to the directory
+    omega_range: list of float or array of float
+        the exported problem will be set up with the following linear range:
+        linspace(min(omega_range), max(omega_range), len(omega_range))
     """
 
     if os.path.isdir(directory_name):
-        warn(f"Exporting problem in existing directory: {directory_name}")
+        warn(f"""Exporting problem in already existing directory: {directory_name}
+             You might be overwriting existing files!""")
     else:
         os.makedirs(directory_name)
 
+    # Export the mesh
     write_MAR(
         os.path.join(directory_name, f'{problem.body.name}.dat'),
         problem.body.vertices,
         problem.body.faces,
-        xOz_symmetry=isinstance(problem.body, ReflectionSymmetry)
+        # xOz_symmetry=isinstance(problem.body, ReflectionSymmetry)
     )
 
+    # Set range of frequencies
     if omega_range is None:
         omega_nb_steps = 1
         omega_start = omega
@@ -36,6 +47,7 @@ def export_as_Nemoh_directory(problem, directory_name, omega_range=None):
         omega_start = min(omega_range)
         omega_stop = max(omega_range)
 
+    # Write Nemoh.cal
     with open(os.path.join(directory_name, "Nemoh.cal"), "w") as nemoh_cal:
         nemoh_cal.write(
                 DEFAULT_NEMOH_CAL.format(
@@ -51,15 +63,17 @@ def export_as_Nemoh_directory(problem, directory_name, omega_range=None):
                     )
                 )
 
+    # Write input.txt
     with open(os.path.join(directory_name, "input.txt"), "w") as input_txt:
         input_txt.write(DEFAULT_INPUT_TXT)
 
+    # Write ID.dat
     with open(os.path.join(directory_name, "ID.dat"), "w") as id_dat:
         id_dat.write(f"1\n.")
 
 
 DEFAULT_NEMOH_CAL = """--- Environment ------------------------------------------------------------------------------------------------------------------
-{rho}			! RHO 			! KG/M**3 	! Fluid specific volume
+{rho}			! RHO			! KG/M**3	! Fluid specific volume
 {g}				! G			! M/S**2	! Gravity
 {depth}			! DEPTH			! M		! Water depth
 0.	0.			! XEFF YEFF		! M		! Wave measurement point
@@ -77,16 +91,16 @@ DEFAULT_NEMOH_CAL = """--- Environment -----------------------------------------
 {omega_nb_steps} {omega_start} {omega_stop} ! Frequencies range
 0	0.	0.		! Number of wave directions, Min and Max (degrees)
 --- Post processing ---------------------------------------------------------------------------------------------------------------
-0	0.1	10.		! IRF 				! IRF calculation (0 for no calculation), time step and duration
+0	0.1	10.		! IRF				! IRF calculation (0 for no calculation), time step and duration
 0				! Show pressure
-0 	0.	180.		! Kochin function 		! Number of directions of calculation (0 for no calculations), Min and Max (degrees)
-0	0	100.	100.	! Free surface elevation 	! Number of points in x direction (0 for no calcutions) and y direction and dimensions of domain in x and y direction
+0	0.	180.		! Kochin function		! Number of directions of calculation (0 for no calculations), Min and Max (degrees)
+0	0	100.	100.	! Free surface elevation	! Number of points in x direction (0 for no calcutions) and y direction and dimensions of domain in x and y direction
 """
 
 DEFAULT_INPUT_TXT="""--- Calculation parameters ------------------------------------------------------------------------------------
-1				! Indiq_solver		! - 		! Solver (0) Direct Gauss (1) GMRES (2) GMRES with FMM acceleration (2 not implemented yet)
-20				! IRES			! - 		! Restart parameter for GMRES
+1				! Indiq_solver		! -		! Solver (0) Direct Gauss (1) GMRES (2) GMRES with FMM acceleration (2 not implemented yet)
+20				! IRES			! -		! Restart parameter for GMRES
 5.E-07				! TOL_GMRES		! -		! Stopping criterion for GMRES
-100				! MAXIT			! - 		! Maximum iterations for GMRES
+100				! MAXIT			! -		! Maximum iterations for GMRES
 1				! Sav_potential		! -		! Save potential for visualization
 """
