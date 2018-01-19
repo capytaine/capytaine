751faf4f3af69df921e15ce9541222bbfb566d41
Matthieu Ancellin <matthieu.ancellin@ucd.ie>
2018-01-19 11:30:07 +0000

    Update version number.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ Wavenumber = Extension(
 
 if __name__ == "__main__":
     setup(name='capytaine',
-          version='0.1',
+          version='0.2',
           description='Nemoh python wrapper',
           url='http://github.com/mancellin/capytaine',
           author='Matthieu Ancellin',
