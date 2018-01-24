d676c1770875986df8bf43d154bd3fb8efe287d9
Matthieu Ancellin <matthieu.ancellin@ucd.ie>
2018-01-24 16:24:09 +0000

    Move non-essential modules into "tools" subdirectory.

---
+++ capytaine/tools/Airy_wave.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# coding: utf-8
+"""
+Compute the potential and velocity of Airy wave.
+"""
+
+import numpy as np
+
+
+def Airy_wave_potential(X, pb):
+    """Compute the potential for Airy waves at a given point (or array of points).
+
+    Parameters
+    ----------
+    X: array of shape (3) or (N x 3)
+        coordinates of the points in which to evaluate the potential.
+    pb: DiffractionProblem
+        problem with the environmental conditions (g, rho, ...) of interest
+
+    Returns
+    -------
+    array of shape (1) or (N x 1)
+        The potential
+    """
+    x, y, z = X.T
+    k = pb.wavenumber
+    h = pb.depth
+    wbar = x*np.cos(pb.angle) + y*np.sin(pb.angle)
+
+    if 0 <= k*h < 20:
+        cih = np.cosh(k*(z+h))/np.cosh(k*h)
+        # sih = np.sinh(k*(z+h))/np.cosh(k*h)
+    else:
+        cih = np.exp(k*z)
+        # sih = np.exp(k*z)
+
+    return -1j*pb.g/pb.omega * cih * np.exp(1j * k * wbar)
+
+
+def Airy_wave_velocity(X, pb):
+    """Compute the fluid velocity for Airy waves at a given point (or array of points).
+
+    Parameters
+    ----------
+    X: array of shape (3) or (N x 3)
+        coordinates of the points in which to evaluate the potential.
+    pb: DiffractionProblem
+        problem with the environmental conditions (g, rho, ...) of interest
+
+    Returns
+    -------
+    array of shape (3) or (N x 3)
+        the velocity vectors
+    """
+    x, y, z = X.T
+    k = pb.wavenumber
+    h = pb.depth
+
+    wbar = x*np.cos(pb.angle) + y*np.sin(pb.angle)
+
+    if 0 <= k*h < 20:
+        cih = np.cosh(k*(z+h))/np.cosh(k*h)
+        sih = np.sinh(k*(z+h))/np.cosh(k*h)
+    else:
+        cih = np.exp(k*z)
+        sih = np.exp(k*z)
+
+    v = pb.g*k/pb.omega * \
+        np.exp(1j * k * wbar) * \
+        np.array([np.cos(pb.angle)*cih, np.sin(pb.angle)*cih, -1j*sih])
+
+    return v.T
