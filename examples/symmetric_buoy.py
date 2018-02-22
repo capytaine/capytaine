e5f8ec776f976cca4afd3c17adb0b540ce3e7ef3
Matthieu Ancellin <matthieu.ancellin@ucd.ie>
2018-02-22 21:54:13 +0000

    Fix symmetric buoy test case.

--- examples/symmetric_buoy.py
+++ examples/symmetric_buoy.py
@@ -11,6 +11,7 @@ import matplotlib.pyplot as plt
 
 from capytaine.reference_bodies import generate_axi_symmetric_body
 from capytaine.problems import RadiationProblem
+from capytaine.results import assemble_dataset
 from capytaine.Nemoh import Nemoh
 
 logging.basicConfig(level=logging.INFO, format="%(levelname)s:\t%(message)s")
@@ -24,22 +25,27 @@ def shape(z):
 #     profile=[[0, 0, -5], [1, 0, -4], [1.5, 0, -3], [2.0, 0, -2], [1.3, 0, -1], [0, 0, -0.5]]
 # )
 
-buoy = generate_axi_symmetric_body(shape, z_range=np.linspace(-5.0, 0.0, 20), nphi=20)
+buoy = generate_axi_symmetric_body(shape, z_range=np.linspace(-5.0, 0.0, 30), nphi=40)
 # buoy.show()
 
 buoy.dofs["Heave"] = buoy.faces_normals @ (0, 0, 1)
 
 solver = Nemoh()
 
-omega_range = np.linspace(0.1, 5.0, 40)
+omega_range = np.linspace(0.1, 5.0, 60)
 
-problems = [RadiationProblem(body=buoy, rho=rho, omega=omega) for omega in omega_range]
+problems = [RadiationProblem(body=buoy, radiating_dof='Heave', rho=rho, omega=omega) for omega in omega_range]
 
 results = [solver.solve(pb) for pb in problems]
-results = np.array(results)
+dataset = assemble_dataset(results)
 
 plt.figure()
-plt.plot(omega_range, results[:, 0, 0, 0]/(rho*buoy.volume), label="Added mass")
-plt.plot(omega_range, results[:, 1, 0, 0]/(rho*buoy.volume), label="Added damping")
+plt.plot(omega_range,
+         dataset['added_mass'].sel(radiating_dof='Heave', influenced_dof='Heave')/(rho*buoy.volume),
+         label="Added mass")
+plt.plot(omega_range,
+         dataset['radiation_damping'].sel(radiating_dof='Heave', influenced_dof='Heave')/(rho*buoy.volume),
+         label="Added damping")
+plt.grid()
 plt.legend()
 plt.show()
