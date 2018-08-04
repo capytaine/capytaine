b2ebc1260e7995ba4efd2ede41c2bb98c7f36b79
Matthieu Ancellin <matthieu.ancellin@ucd.ie>
2018-08-04 17:05:45 +0100

    Fix Travis auto-upload of binaries.

--- conda/conda_upload.sh
+++ conda/conda_upload.sh
@@ -1,1 +1,1 @@
-anaconda -t $CONDA_UPLOAD_TOKEN upload -u mancellin -l nightly $HOME/miniconda/conda-bld/linux-64/capytaine*.tar.bz2
+anaconda -t $CONDA_UPLOAD_TOKEN upload -u mancellin -l nightly $HOME/miniconda/conda-bld/linux-64/capytaine*.tar.bz2 --force
