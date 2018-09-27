1a685f6f32d3a627278101bf6b91ab50f4f33d5c
Matthieu Ancellin <matthieu.ancellin@ucd.ie>
2018-09-27 13:48:57 +0100

    Plot block matrices.

--- capytaine/matrices/block_matrices.py
+++ capytaine/matrices/block_matrices.py
@@ -2,19 +2,18 @@
 # coding: utf-8
 
 import logging
+from itertools import cycle
 
 import numpy as np
-
-import matplotlib.pyplot as plt
-from matplotlib.patches import Rectangle
-from matplotlib.collections import PatchCollection
 
 
 class BlockMatrix:
     ndim = 2  # Other dimensions have not been implemented.
+    COLORS = cycle([f'C{i}' for i in range(10)])
 
     def __init__(self, blocks):
         # TODO: Check dimensionality
+        # Every
         self.blocks = np.asanyarray(blocks)
 
     @property
@@ -41,22 +40,31 @@ class BlockMatrix:
         return np.block(full_blocks)
 
     def plot_shape(self):
-        fig = plt.figure()
-        coll = PatchCollection(self._patches(np.zeros((2,))))
-        plt.gca().add_collection(coll)
-        # plt.axis('off')
+        import matplotlib.pyplot as plt
+        plt.figure()
+        for patch in self._patches(np.zeros((2,))):
+            plt.gca().add_patch(patch)
+        plt.axis('equal')
+        plt.xlim(0, self.shape[1])
+        plt.ylim(0, self.shape[0])
+        plt.gca().invert_yaxis()
         plt.show()
 
     def _patches(self, global_shift):
+        from matplotlib.patches import Rectangle
         patches = []
         shift = global_shift.copy()
         for line in self.blocks:
             shift[0] = global_shift[0]
             for block in line:
-                if isinstance(block, np.ndarray):
-                    patches.append(Rectangle(shift, block.shape[0], block.shape[1]))
-                    shift[0] += block.shape[0]
-            shift[0] += block.shape[1]
+                if isinstance(block, BlockMatrix):
+                    patches.extend(block._patches(shift))
+                elif isinstance(block, np.ndarray):
+                    patches.append(Rectangle(shift, block.shape[1], block.shape[0], edgecolor='k', facecolor=next(self.COLORS)))
+                else:
+                    raise AttributeError()
+                shift[0] += block.shape[1]
+            shift[1] += line[0].shape[0]
         return patches
 
 
@@ -95,27 +103,27 @@ if __name__ == "__main__":
         [np.random.rand(2, 2), np.zeros((2, 2))],
         [np.zeros((2, 2)), np.random.rand(2, 2)]
     ])
-    print(repr(A))
-    print(A.full_matrix())
+    # print(repr(A))
+    # print(A.full_matrix())
     A.plot_shape()
-
 
     # I = identity_like(A)
     # print(repr(I))
     # print(I.full_matrix())
 
-    # B = BlockMatrix([[A, np.zeros((4, 1))]])
-    # print(repr(B))
-    # print(B.full_matrix())
+    B = BlockMatrix([[A, np.zeros((4, 1))]])
+    print(repr(B))
+    print(B.full_matrix())
+    B.plot_shape()
 
     # O = zeros_like(B)
     # print(repr(O))
     # print(O.full_matrix())
 
-    # C = BlockMatrix([
-    #     [np.random.rand(3, 3), np.random.rand(3, 1)],
-    #     [np.random.rand(1, 3), np.random.rand(1, 1)]
-    # ])
-    # print(repr(C))
-    # print(C.full_matrix())
-
+    C = BlockMatrix([
+        [np.random.rand(3, 3), np.random.rand(3, 1)],
+        [np.random.rand(1, 3), np.random.rand(1, 1)]
+    ])
+    print(repr(C))
+    print(C.full_matrix())
+    C.plot_shape()
--- capytaine/matrices/block_toeplitz_matrices.py
+++ capytaine/matrices/block_toeplitz_matrices.py
@@ -14,7 +14,11 @@ class BlockSymmetricToeplitzMatrix(BlockMatrix):
 
     @property
     def nb_blocks(self):
-        return (self.t_blocks.shape[0], self.t_blocks.shape[0])
+        return self.t_blocks.shape[0], self.t_blocks.shape[0]
+
+    @property
+    def block_shape(self):
+        return self.t_blocks[0].shape
 
     @property
     def shape(self):
@@ -32,7 +36,31 @@ class BlockSymmetricToeplitzMatrix(BlockMatrix):
 
     def _index_grid(self):
         n = self.nb_blocks[0]
-        return np.fromfunction(lambda i, j: abs(i-j), (n, n), dtype=np.int)
+        return [[abs(i-j) for i in range(n)] for j in range(n)]
+
+    def _positions_of_index(self, k):
+        n = self.nb_blocks[0]
+        return [(i, j) for i in range(1, n) for j in range(n) if abs(i-j) == k]
+
+    def _patches(self, global_shift):
+        from matplotlib.patches import Rectangle
+        patches = []
+        for k, block in enumerate(self.t_blocks):
+            shift = np.array((global_shift[0] + k*self.block_shape[1], global_shift[1]))
+            if isinstance(block, BlockMatrix):
+                patches_of_block = block._patches(shift)
+            elif isinstance(block, np.ndarray):
+                patches_of_block = [Rectangle(shift, self.block_shape[1], self.block_shape[0], edgecolor='k', facecolor=next(self.COLORS))]
+            else:
+                raise AttributeError()
+
+            patches.extend(patches_of_block)
+            for i, j in self._positions_of_index(k):
+                for patch in patches_of_block:
+                    local_shift = np.array(patch.get_xy()) + np.array(((j-k)*self.block_shape[1], i*self.block_shape[0]))
+                    patches.append(Rectangle(local_shift, patch.get_width(), patch.get_height(), facecolor=patch.get_facecolor(), alpha=0.5))
+
+        return patches
 
 
 if __name__ == "__main__":
@@ -43,9 +71,19 @@ if __name__ == "__main__":
     print(A.nb_blocks)
     print(A.shape)
     print(A.full_matrix())
+    A.plot_shape()
 
-    B = BlockSymmetricToeplitzMatrix([A, A.T])
-
+    new_block = lambda: BlockMatrix([
+        [np.random.rand(2, 2), np.random.rand(2, 2)],
+        [np.random.rand(2, 2), np.random.rand(2, 2)],
+    ])
+    B = BlockSymmetricToeplitzMatrix([new_block(), new_block()])
     print(B.t_blocks)
     print(B.shape)
+    B.plot_shape()
 
+    new_block = lambda: BlockSymmetricToeplitzMatrix([np.random.rand(2, 2), np.random.rand(2, 2)])
+    C = BlockSymmetricToeplitzMatrix([new_block(), new_block()])
+    print(C.t_blocks)
+    print(C.shape)
+    C.plot_shape()
