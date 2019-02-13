8a5e00a2acf2bf96684feff748e56eda3b7984f8
Matthieu Ancellin <matthieu.ancellin@ucd.ie>
2019-02-13 18:07:25 +0000

    Add a few import shortcuts.

---
+++ capytaine/tools/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from capytaine.tools.rao import rao
+from capytaine.tools.kochin import compute_Kochin, kochin_data_array, kochin_dataset
+from capytaine.tools.Airy_wave import Airy_wave_velocity, Airy_wave_potential, Froude_Krylov_force
--- capytaine/ui/vtk/__init__.py
+++ capytaine/ui/vtk/__init__.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-
+from capytaine.ui.vtk.mesh_viewer import MeshViewer
+from capytaine.ui.vtk.body_viewer import FloatingBodyViewer
+from capytaine.ui.vtk.animation import Animation
