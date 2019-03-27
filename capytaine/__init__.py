96c8a361131d41705107fa3ff3382260f93360b8
Matthieu Ancellin <matthieu.ancellin@ucd.ie>
2019-03-27 20:58:04 +0000

    Update version number.

--- capytaine/__init__.py
+++ capytaine/__init__.py
@@ -4,7 +4,7 @@
 # See LICENSE file at <https://github.com/mancellin/capytaine>
 
 __author__ = 'Matthieu Ancellin'
-__version__ = '1.0'
+__version__ = '1.0.1'
 __license__ = 'GPL-3.0'
 
 from capytaine.meshes.geometry import Axis, Plane, xOz_Plane, yOz_Plane, xOy_Plane
