9f299b8e6d64b64d8c8dde1d086c7a6a782be0e7
Matthieu Ancellin <matthieu.ancellin@cmla.ens-cachan.fr>
2019-08-23 15:35:29 +0200

    Fix and reorganize tests after new solver structure.

--- pytest/test_bem_solver.py
+++ pytest/test_bem_solver.py
@@ -26,7 +26,7 @@ def test_exportable_settings():
     assert engine.exportable_settings['matrix_cache_size'] == 0
     assert engine.exportable_settings['linear_solver'] == 'gmres'
 
-    solver = BEMSolver(green_functions=gf, engine=engine)
+    solver = BEMSolver(green_function=gf, engine=engine)
     assert solver.exportable_settings['green_function'] == 'Delhommeau'
     assert solver.exportable_settings['tabulation_nb_integration_points'] == 50
     assert solver.exportable_settings['finite_depth_prony_decomposition_method'] == 'fortran'
@@ -35,70 +35,9 @@ def test_exportable_settings():
     assert solver.exportable_settings['linear_solver'] == 'gmres'
 
 
-def test_cache_matrices():
-    """Test how the solver caches the interaction matrices."""
-    params_1 = dict(free_surface=0.0, sea_bottom=-np.infty, wavenumber=1.0)
-    params_2 = dict(free_surface=0.0, sea_bottom=-np.infty, wavenumber=2.0)
-
-    # No cache
-    solver = Nemoh(matrix_cache_size=0)
-    S, K = solver.build_matrices(sphere.mesh, sphere.mesh, **params_1)
-    S_again, K_again = solver.build_matrices(sphere.mesh, sphere.mesh, **params_1)
-    assert S is not S_again
-    assert K is not K_again
-
-    # Cache
-    solver = Nemoh(matrix_cache_size=1)
-    S, K = solver.build_matrices(sphere.mesh, sphere.mesh, **params_1)
-    S_again, K_again = solver.build_matrices(sphere.mesh, sphere.mesh, **params_1)
-    _, _ = solver.build_matrices(sphere.mesh, sphere.mesh, **params_2)
-    S_once_more, K_once_more = solver.build_matrices(sphere.mesh, sphere.mesh, **params_1)
-    assert S is S_again
-    assert S is not S_once_more
-    assert K is K_again
-    assert K is not K_once_more
-
-
-def test_cache_rankine_matrices():
-    """Test how the solver caches the Rankine part of the interaction matrices."""
-    params_1 = dict(free_surface=0.0, sea_bottom=-np.infty, wavenumber=1.0)
-
-    # No cache of rankine matrices
-    solver = Nemoh(matrix_cache_size=1, cache_rankine_matrices=True)
-    Sr, Vr = solver.build_matrices_rankine(sphere.mesh, sphere.mesh, **params_1)
-    Sr_again, Vr_again = solver.build_matrices_rankine(sphere.mesh, sphere.mesh, **params_1)
-    assert Sr is Sr_again
-    assert Vr is Vr_again
-
-    # Cache of rankine matrices
-    solver = Nemoh(matrix_cache_size=1)
-    Sr, Vr = solver.build_matrices_rankine(sphere.mesh, sphere.mesh, **params_1)
-    Sr_again, Vr_again = solver.build_matrices_rankine(sphere.mesh, sphere.mesh, **params_1)
-    assert Sr is not Sr_again
-    assert Vr is not Vr_again
-
-
-def test_custom_linear_solver():
-    """Solve a simple problem with a custom linear solver."""
-    problem = RadiationProblem(body=sphere, omega=1.0, sea_bottom=-np.infty)
-
-    reference_solver = Nemoh(linear_solver="gmres", matrix_cache_size=0, hierarchical_matrices=False)
-    reference_result = reference_solver.solve(problem)
-
-    def my_linear_solver(A, b):
-        """A dumb solver for testing."""
-        return np.linalg.inv(A) @ b
-
-    my_bem_solver = Nemoh(linear_solver=my_linear_solver, matrix_cache_size=0, hierarchical_matrices=False)
-    assert 'my_linear_solver' in my_bem_solver.exportable_settings()['linear_solver']
-
-    result = my_bem_solver.solve(problem)
-    assert np.isclose(reference_result.added_masses['Surge'], result.added_masses['Surge'])
-
-
 def test_limit_frequencies():
     """Test if how the solver answers when asked for frequency of 0 or ∞."""
-    solver = Nemoh()
+    solver = BEMSolver()
 
     solver.solve(RadiationProblem(body=sphere, omega=0.0, sea_bottom=-np.infty))
 
@@ -112,7 +51,7 @@ def test_limit_frequencies():
 
 
 def test_fill_dataset():
-    solver = Nemoh()
+    solver = BEMSolver()
     test_matrix = xr.Dataset(coords={
         'omega': np.linspace(0.1, 4.0, 3),
         'wave_direction': np.linspace(0.0, pi, 3),
@@ -136,7 +75,6 @@ def test_fill_dataset():
     # Test the attributes
     assert dataset.attrs['capytaine_version'] == __version__
     assert 'start_of_computation' in dataset.attrs
-    assert 'cache_rankine_matrices' in dataset.attrs
     assert 'incoming_waves_convention' in dataset.attrs
 
     # Try to strip out the outputs and recompute
@@ -148,7 +86,7 @@ def test_fill_dataset():
 
 
 def test_fill_dataset_with_kochin_functions():
-    solver = Nemoh()
+    solver = BEMSolver()
     test_matrix = xr.Dataset(coords={
         'omega': [1.0],
         'theta': np.linspace(0, 2*pi, 5),
