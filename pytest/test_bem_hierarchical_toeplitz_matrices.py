c60e2c0bc89f658e9a508cd48f844219b9e266d3
Matthieu Ancellin <matthieu.ancellin@cmla.ens-cachan.fr>
2019-09-03 11:09:15 +0200

    Fix tests.

--- pytest/test_bem_hierarchical_toeplitz_matrices.py
+++ pytest/test_bem_hierarchical_toeplitz_matrices.py
@@ -158,7 +158,7 @@ def test_low_rank_matrices():
     two_distant_buoys = FloatingBody.join_bodies(buoy, buoy.translated_x(20))
     two_distant_buoys.mesh._meshes[1].name = "other_buoy_mesh"
 
-    S, V = solver_with_sym.build_matrices(two_distant_buoys.mesh, two_distant_buoys.mesh, 0.0, -np.infty, 1.0, solver_with_sym.green_function)
+    S, V = solver_with_sym.build_matrices(two_distant_buoys.mesh, two_distant_buoys.mesh, 0.0, -np.infty, 1.0)
     assert isinstance(S.all_blocks[0, 1], LowRankMatrix)
     assert isinstance(S.all_blocks[1, 0], LowRankMatrix)
     # S.plot_shape()
@@ -203,8 +203,8 @@ def test_array_of_spheres():
     nemoh_without_sym = Nemoh(hierarchical_matrices=False, **settings)
     nemoh_with_sym = Nemoh(hierarchical_matrices=True, **settings)
 
-    fullS, fullV = nemoh_without_sym.build_matrices(array.mesh, array.mesh, 0.0, -np.infty, 1.0, nemoh_without_sym.green_function)
-    S, V = nemoh_with_sym.build_matrices(array.mesh, array.mesh, 0.0, -np.infty, 1.0, nemoh_without_sym.green_function)
+    fullS, fullV = nemoh_without_sym.build_matrices(array.mesh, array.mesh, 0.0, -np.infty, 1.0)
+    S, V = nemoh_with_sym.build_matrices(array.mesh, array.mesh, 0.0, -np.infty, 1.0)
 
     assert isinstance(S, cpt.matrices.block.BlockMatrix)
     assert np.allclose(S.full_matrix(), fullS)
