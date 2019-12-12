capytaine/capytaine

commit d3e308a1d998328f86471d6c5019db8472877fd3

Add kochin_diffraction to fill_dataset returns.
Previously, only the radiation cases where considered.
Fixing issue #22

capytaine/bem/solver.py
@@ -143,9 +143,10 @@ class BEMSolver:
         problems = problems_from_dataset(dataset, bodies)
         if 'theta' in dataset.coords:
             results = self.solve_all(problems, keep_details=True)
-            kochin = kochin_data_array(results, dataset.coords['theta'])
+            kochin_rad, kochin_diff= kochin_data_array(results, dataset.coords['theta'])
             dataset = assemble_dataset(results, attrs=attrs, **kwargs)
-            dataset['kochin'] = kochin
+            dataset['kochin'] = kochin_rad
+            dataset['kochin_diffraction'] = kochin_diff
         else:
             results = self.solve_all(problems, keep_details=False)
             dataset = assemble_dataset(results, attrs=attrs, **kwargs)

capytaine/io/xarray.py
@@ -181,14 +181,28 @@ def kochin_data_array(results: Sequence[LinearPotentialFlowResult],
         :meth:`~capytaine.post_pro.kochin.compute_kochin`
             The present function is just a wrapper around :code:`compute_kochin`.
     """
-    records = pd.DataFrame([dict(result.settings_dict, theta=theta, kochin=kochin)
-                            for result in results
-                            for theta, kochin in zip(theta_range.data, compute_kochin(result, theta_range, **kwargs))])
+    records = pd.DataFrame([
+        dict(result.settings_dict, theta=theta, kochin=kochin)
+        for result in results
+        for theta, kochin in zip(theta_range.data,
+                                 compute_kochin(result, theta_range, **kwargs))
+    ])
 
-    ds = _dataset_from_dataframe(records, ['kochin'],
-                                 dimensions=['omega', 'radiating_dof', 'theta'],
-                                 optional_dims=['g', 'rho', 'body_name', 'water_depth'])
-    return ds['kochin']
+    diffraction = _dataset_from_dataframe(
+        records[~records['wave_direction'].isnull()],
+        ['kochin'],
+        dimensions=['omega', 'wave_direction', 'theta'],
+        optional_dims=['g', 'rho', 'body_name', 'water_depth']
+    )
+
+    radiation = _dataset_from_dataframe(
+        records[~records['radiating_dof'].isnull()],
+        variables=['kochin'],
+        dimensions=['omega', 'radiating_dof', 'theta'],
+        optional_dims=['g', 'rho', 'body_name', 'water_depth']
+    )
+
+    return diffraction['kochin'], radiation['kochin']
 
 
 def assemble_dataset(results: Sequence[LinearPotentialFlowResult],

pytest/test_post_pro_kochin.py
@@ -1,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+"""Test related to the computation of Kochin functions."""
+
+import numpy as np
+from numpy import pi
+import xarray as xr
+
+import capytaine as cpt
+
+def test_kochin_sphere():
+    sphere = cpt.Sphere(clip_free_surface=True)
+    sphere.add_translation_dof(name="Heave")
+
+    test_matrix = xr.Dataset(coords={
+        'omega': [1.0],
+        'wave_direction': [0.0, pi/2],
+        'radiating_dof': ["Heave"],
+        'theta': [0.0, pi/2, pi, 3*pi/2],
+    })
+    ds = cpt.BEMSolver().fill_dataset(test_matrix, [sphere])
+
+    assert 'kochin' in ds
+    assert 'kochin_diffraction' in ds
+    # return ds
+
+# ds = test_kochin_sphere()
+# print(ds)

commit b18a99b5b323b1cac9972d88583680cdc96a978b

Correct broken implementation in d3e308a1.
Really fix issue #22.

capytaine/bem/solver.py
@@ -143,10 +143,9 @@ class BEMSolver:
         problems = problems_from_dataset(dataset, bodies)
         if 'theta' in dataset.coords:
             results = self.solve_all(problems, keep_details=True)
-            kochin_rad, kochin_diff= kochin_data_array(results, dataset.coords['theta'])
+            kochin = kochin_data_array(results, dataset.coords['theta'])
             dataset = assemble_dataset(results, attrs=attrs, **kwargs)
-            dataset['kochin'] = kochin_rad
-            dataset['kochin_diffraction'] = kochin_diff
+            dataset.update(kochin)
         else:
             results = self.solve_all(problems, keep_details=False)
             dataset = assemble_dataset(results, attrs=attrs, **kwargs)

capytaine/io/xarray.py
@@ -188,21 +188,27 @@ def kochin_data_array(results: Sequence[LinearPotentialFlowResult],
                                  compute_kochin(result, theta_range, **kwargs))
     ])
 
-    diffraction = _dataset_from_dataframe(
-        records[~records['wave_direction'].isnull()],
-        ['kochin'],
-        dimensions=['omega', 'wave_direction', 'theta'],
-        optional_dims=['g', 'rho', 'body_name', 'water_depth']
-    )
+    kochin_data = {}
 
-    radiation = _dataset_from_dataframe(
-        records[~records['radiating_dof'].isnull()],
-        variables=['kochin'],
-        dimensions=['omega', 'radiating_dof', 'theta'],
-        optional_dims=['g', 'rho', 'body_name', 'water_depth']
-    )
+    if 'wave_direction' in records.columns:
+        diffraction = _dataset_from_dataframe(
+            records[~records['wave_direction'].isnull()],
+            ['kochin'],
+            dimensions=['omega', 'wave_direction', 'theta'],
+            optional_dims=['g', 'rho', 'body_name', 'water_depth']
+        )
+        kochin_data['kochin_diffraction'] = diffraction['kochin']
 
-    return diffraction['kochin'], radiation['kochin']
+    if 'radiating_dof' in records.columns:
+        radiation = _dataset_from_dataframe(
+            records[~records['radiating_dof'].isnull()],
+            variables=['kochin'],
+            dimensions=['omega', 'radiating_dof', 'theta'],
+            optional_dims=['g', 'rho', 'body_name', 'water_depth']
+        )
+        kochin_data['kochin'] = radiation['kochin']
+
+    return kochin_data
 
 
 def assemble_dataset(results: Sequence[LinearPotentialFlowResult],

pytest/test_bem_solver.py
@@ -88,18 +88,6 @@ def test_fill_dataset():
     assert np.allclose(recomputed_dataset["added_mass"].data, dataset["added_mass"].data)
 
 
-def test_fill_dataset_with_kochin_functions():
-    solver = BEMSolver()
-    test_matrix = xr.Dataset(coords={
-        'omega': [1.0],
-        'theta': np.linspace(0, 2*pi, 5),
-        'radiating_dof': list(sphere.dofs.keys()),
-    })
-    ds = solver.fill_dataset(test_matrix, [sphere])
-    assert 'theta' in ds.coords
-    assert 'kochin' in ds
-
-
 # TODO: move the code below to test_io_xarray.py
     # wavenumbers = wavenumber_data_array(results)
     # assert isinstance(wavenumbers, xr.DataArray)

pytest/test_post_pro_kochin.py
@@ -8,21 +8,37 @@ import xarray as xr
 
 import capytaine as cpt
 
-def test_kochin_sphere():
+
+def test_fill_dataset_with_kochin_functions():
     sphere = cpt.Sphere(clip_free_surface=True)
     sphere.add_translation_dof(name="Heave")
+    solver = cpt.BEMSolver()
 
     test_matrix = xr.Dataset(coords={
         'omega': [1.0],
-        'wave_direction': [0.0, pi/2],
+        'theta': [0.0, pi/2],
         'radiating_dof': ["Heave"],
-        'theta': [0.0, pi/2, pi, 3*pi/2],
     })
-    ds = cpt.BEMSolver().fill_dataset(test_matrix, [sphere])
+    ds = solver.fill_dataset(test_matrix, [sphere])
+    assert 'theta' in ds.coords
+    assert 'kochin' in ds
+    assert 'kochin_diffraction' not in ds
 
+    # Because of the symmetries of the body
+    assert np.isclose(ds['kochin'].sel(radiating_dof="Heave", theta=0.0),
+                      ds['kochin'].sel(radiating_dof="Heave", theta=pi/2))
+
+    test_matrix = xr.Dataset(coords={
+        'omega': [1.0],
+        'radiating_dof': ["Heave"],
+        'wave_direction': [-pi/2, 0.0],
+        'theta': [0.0, pi/2],
+    })
+    ds = solver.fill_dataset(test_matrix, [sphere])
+    assert 'theta' in ds.coords
     assert 'kochin' in ds
     assert 'kochin_diffraction' in ds
-    # return ds
 
-# ds = test_kochin_sphere()
-# print(ds)
+    # Because of the symmetries of the body
+    assert np.isclose(ds['kochin_diffraction'].sel(wave_direction=-pi/2, theta=0.0),
+                      ds['kochin_diffraction'].sel(wave_direction=0.0, theta=pi/2))