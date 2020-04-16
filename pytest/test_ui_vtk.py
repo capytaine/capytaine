d77ccfb2cd61f5840311727816b58dc535d2de52
Matthieu Ancellin <matthieu.ancellin@ens-paris-saclay.fr>
2020-04-16 10:54:38 +0200

    Do not test for vtk if not installed.

--- pytest/test_ui_vtk.py
+++ pytest/test_ui_vtk.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+from warnings import warn
+
 import capytaine as cpt
 
 def test_animation_of_dofs():
     body = cpt.Sphere()
     body.add_translation_dof(name="Heave")
-    animation = body.animate({"Heave": 0.2}, loop_duration=1.0)
-    animation.embed_in_notebook()
+    try:
+        animation = body.animate({"Heave": 0.2}, loop_duration=1.0)
+        animation.embed_in_notebook()
+    except ImportError:
+        warn("VTK is not installed and thus has not been tested.")
 
