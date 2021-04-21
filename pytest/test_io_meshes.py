021347600c5d16994b734e453fa588f28ad04e8b
Ryan Coe <rcoe@sandia.gov>
2021-04-21 09:01:59 -0600

    better (and actually working!) method of handling optional dependencies

--- pytest/test_io_meshes.py
+++ pytest/test_io_meshes.py
@@ -7,13 +7,17 @@ import pytest
 
 import numpy as np
 import xarray as xr
-import pygmsh
 
 from capytaine.io.mesh_writers import write_STL
 from capytaine.io.mesh_loaders import load_STL
 from capytaine.bodies.predefined import Sphere
 from capytaine import FloatingBody
 from capytaine import BEMSolver
+
+try:
+    import pygmsh
+except ImportError:
+    pygmsh = None
 
 offset=1e-2 # small offset so you can clip at z=0
 
@@ -23,7 +27,6 @@ rho_water = 1e3
 
 def test_STL(tmp_path):
     try:
-
         mesh = Sphere().mesh.merged()
         filepath = tmp_path / "test.stl"
         write_STL(filepath, mesh.vertices, mesh.faces)
@@ -36,113 +39,114 @@ def test_STL(tmp_path):
     except ImportError:
         warn("VTK is not installed and thus has not been tested.")
 
+
+
+@pytest.mark.pygmsh_functions
+@pytest.mark.skipif(pygmsh is None,
+                    reason='pygmsh and/or meshio is not installed')
 def test_from_meshio_pygmsh_WaveBot():
-    try:
-        T1=0.16
-        T2=0.37
-        r1=0.88
-        r2=0.35
-        with pygmsh.occ.Geometry() as geom:
-                cyl = geom.add_cylinder([0,0,0], [0,0,-T1], r1)
-                cone = geom.add_cone([0,0,-T1], [0,0,-T2], r1, r2)
-                geom.translate(cyl, [0, 0, offset])
-                geom.translate(cone, [0, 0, offset])
-                geom.boolean_union([cyl, cone])       
-                mesh = geom.generate_mesh(dim=2)
-        fb = FloatingBody.from_meshio(mesh)
+    T1=0.16
+    T2=0.37
+    r1=0.88
+    r2=0.35
+    with pygmsh.occ.Geometry() as geom:
+            cyl = geom.add_cylinder([0,0,0], [0,0,-T1], r1)
+            cone = geom.add_cone([0,0,-T1], [0,0,-T2], r1, r2)
+            geom.translate(cyl, [0, 0, offset])
+            geom.translate(cone, [0, 0, offset])
+            geom.boolean_union([cyl, cone])       
+            mesh = geom.generate_mesh(dim=2)
+    fb = FloatingBody.from_meshio(mesh)
 
-        vol = fb.mesh.volume
-        vol_exp = np.pi*T1*r1**2 + 1/3 * np.pi * T2 * (r2**2 + r2 * r1 + r1**2)
+    vol = fb.mesh.volume
+    vol_exp = np.pi*T1*r1**2 + 1/3 * np.pi * T2 * (r2**2 + r2 * r1 + r1**2)
 
-        assert pytest.approx(vol_exp, rel=1e-1) == vol
+    assert pytest.approx(vol_exp, rel=1e-1) == vol
 
-        fb.add_translation_dof((0,0,1))
+    fb.add_translation_dof((0,0,1))
 
-        test_matrix = xr.Dataset(coords={
-            'rho': rho_water,
-            'water_depth': [np.infty],          
-            'omega': omega,
-            'wave_direction': 0,
-            'radiating_dof': list(fb.dofs.keys()),
-            })
+    test_matrix = xr.Dataset(coords={
+        'rho': rho_water,
+        'water_depth': [np.infty],          
+        'omega': omega,
+        'wave_direction': 0,
+        'radiating_dof': list(fb.dofs.keys()),
+        })
 
-        solver = BEMSolver()
-        data = solver.fill_dataset(test_matrix, 
-                                bodies=[fb],
-                                hydrostatics=True,
-                                mesh=True,
-                                wavelength=True,
-                                wavenumber=True)
-    except ImportError:
-        warn('pygmsh and/or meshio is not installed and thus from_meshio has not been tested')
+    solver = BEMSolver()
+    data = solver.fill_dataset(test_matrix, 
+                            bodies=[fb],
+                            hydrostatics=True,
+                            mesh=True,
+                            wavelength=True,
+                            wavenumber=True)
 
+
+@pytest.mark.pygmsh_functions
+@pytest.mark.skipif(pygmsh is None,
+                    reason='pygmsh and/or meshio is not installed')
 def test_from_meshio_pygmsh_Cylinder():
-    try:
-        T=0.52
-        r1=0.88
-        with pygmsh.occ.Geometry() as geom:
-                cyl = geom.add_cylinder([0,0,0], [0,0,-T], r1)
-                geom.translate(cyl, [0, 0, offset])    
-                mesh = geom.generate_mesh(dim=2)
-        fb = FloatingBody.from_meshio(mesh)
+    T=0.52
+    r1=0.88
+    with pygmsh.occ.Geometry() as geom:
+            cyl = geom.add_cylinder([0,0,0], [0,0,-T], r1)
+            geom.translate(cyl, [0, 0, offset])    
+            mesh = geom.generate_mesh(dim=2)
+    fb = FloatingBody.from_meshio(mesh)
 
-        vol = fb.mesh.volume
-        vol_exp = np.pi*r1**2*T
+    vol = fb.mesh.volume
+    vol_exp = np.pi*r1**2*T
 
-        assert pytest.approx(vol_exp, rel=1e-1) == vol
+    assert pytest.approx(vol_exp, rel=1e-1) == vol
 
-        fb.add_translation_dof((0,0,1))
+    fb.add_translation_dof((0,0,1))
 
-        test_matrix = xr.Dataset(coords={
-            'rho': rho_water,
-            'water_depth': [np.infty],          
-            'omega': omega,
-            'wave_direction': 0,
-            'radiating_dof': list(fb.dofs.keys()),
-            })
+    test_matrix = xr.Dataset(coords={
+        'rho': rho_water,
+        'water_depth': [np.infty],          
+        'omega': omega,
+        'wave_direction': 0,
+        'radiating_dof': list(fb.dofs.keys()),
+        })
 
-        solver = BEMSolver()
-        data = solver.fill_dataset(test_matrix, 
-                                bodies=[fb],
-                                hydrostatics=True,
-                                mesh=True,
-                                wavelength=True,
-                                wavenumber=True)
-    except ImportError:
-        warn('pygmsh and/or meshio is not installed and thus from_meshio has not been tested')
+    solver = BEMSolver()
+    data = solver.fill_dataset(test_matrix, 
+                            bodies=[fb],
+                            hydrostatics=True,
+                            mesh=True,
+                            wavelength=True,
+                            wavenumber=True)
 
+@pytest.mark.pygmsh_functions
+@pytest.mark.skipif(pygmsh is None,
+                    reason='pygmsh and/or meshio is not installed')
 def test_from_meshio_pygmsh_Sphere():
-    try:
-        r1=0.88
-        with pygmsh.occ.Geometry() as geom:
-                sphere = geom.add_ball(center=[0,0,0], radius=r1)
-                geom.translate(sphere, [0, 0, offset])    
-                mesh = geom.generate_mesh(dim=2)
-        fb = FloatingBody.from_meshio(mesh)
+    r1=0.88
+    with pygmsh.occ.Geometry() as geom:
+            sphere = geom.add_ball(center=[0,0,0], radius=r1)
+            geom.translate(sphere, [0, 0, offset])    
+            mesh = geom.generate_mesh(dim=2)
+    fb = FloatingBody.from_meshio(mesh)
 
-        vol = fb.mesh.volume
-        vol_exp = 1/2*np.pi*4/3*r1**3
+    vol = fb.mesh.volume
+    vol_exp = 1/2*np.pi*4/3*r1**3
 
-        assert pytest.approx(vol_exp, rel=1e-1) == vol
+    assert pytest.approx(vol_exp, rel=1e-1) == vol
 
-        fb.add_translation_dof((0,0,1))
+    fb.add_translation_dof((0,0,1))
 
-        test_matrix = xr.Dataset(coords={
-            'rho': rho_water,
-            'water_depth': [np.infty],          
-            'omega': omega,
-            'wave_direction': 0,
-            'radiating_dof': list(fb.dofs.keys()),
-            })
+    test_matrix = xr.Dataset(coords={
+        'rho': rho_water,
+        'water_depth': [np.infty],          
+        'omega': omega,
+        'wave_direction': 0,
+        'radiating_dof': list(fb.dofs.keys()),
+        })
 
-        solver = BEMSolver()
-        data = solver.fill_dataset(test_matrix, 
-                                bodies=[fb],
-                                hydrostatics=True,
-                                mesh=True,
-                                wavelength=True,
-                                wavenumber=True)
-    except ImportError:
-        warn('pygmsh and/or meshio is not installed and thus from_meshio has not been tested')
-
-
+    solver = BEMSolver()
+    data = solver.fill_dataset(test_matrix, 
+                            bodies=[fb],
+                            hydrostatics=True,
+                            mesh=True,
+                            wavelength=True,
+                            wavenumber=True)
