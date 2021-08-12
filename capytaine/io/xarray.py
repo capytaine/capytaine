2d1be0434b6343808ac63ca810f9dce8a032ebd5
Carlos A. Michelén Ströfer <cmichel@sandia.gov>
2021-08-12 01:02:55 -0600

    Support setting g in fill_dataset (#86)

--- capytaine/io/xarray.py
+++ capytaine/io/xarray.py
@@ -57,6 +57,7 @@ def problems_from_dataset(dataset: xr.Dataset,
     omega_range = dataset['omega'].data if 'omega' in dataset else [_default_parameters['omega']]
     water_depth_range = dataset['water_depth'].data if 'water_depth' in dataset else [_default_parameters['water_depth']]
     rho_range = dataset['rho'].data if 'rho' in dataset else [_default_parameters['rho']]
+    g_range = dataset['g'].data if 'g' in dataset else [_default_parameters['g']]
 
     wave_direction_range = dataset['wave_direction'].data if 'wave_direction' in dataset else None
     radiating_dofs = dataset['radiating_dof'].data.astype(object) if 'radiating_dof' in dataset else None
@@ -72,19 +73,19 @@ def problems_from_dataset(dataset: xr.Dataset,
 
     problems = []
     if wave_direction_range is not None:
-        for omega, wave_direction, water_depth, body_name, rho \
-                in product(omega_range, wave_direction_range, water_depth_range, body_range, rho_range):
+        for omega, wave_direction, water_depth, body_name, rho, g \
+                in product(omega_range, wave_direction_range, water_depth_range, body_range, rho_range, g_range):
             problems.append(
                 DiffractionProblem(body=body_range[body_name], omega=omega,
-                                   wave_direction=wave_direction, sea_bottom=-water_depth, rho=rho)
+                                   wave_direction=wave_direction, sea_bottom=-water_depth, rho=rho, g=g)
             )
 
     if radiating_dofs is not None:
-        for omega, radiating_dof, water_depth, body_name, rho \
-                in product(omega_range, radiating_dofs, water_depth_range, body_range, rho_range):
+        for omega, radiating_dof, water_depth, body_name, rho, g \
+                in product(omega_range, radiating_dofs, water_depth_range, body_range, rho_range, g_range):
             problems.append(
                 RadiationProblem(body=body_range[body_name], omega=omega,
-                                 radiating_dof=radiating_dof, sea_bottom=-water_depth, rho=rho)
+                                 radiating_dof=radiating_dof, sea_bottom=-water_depth, rho=rho, g=g)
             )
 
     return sorted(problems)
