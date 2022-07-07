capytaine/capytaine

commit 39df584a2731d2c438f9f8cf54a60d4eefaf00fd

Minor improvements of Fortran code (#158)
* Uniformize some function signatures
* Uniformize some variable names
* Encode infinite depth as actual infinite number instead of zero.
* Add some 'pure' and 'do concurrent' statements
* Remove unused branch `if` branch in numerical integration of Delhommeau's integrals.

capytaine/green_functions/Delhommeau_f90/Green_wave.f90
@@ -11,13 +11,13 @@ MODULE GREEN_WAVE
   ! Dependencies between the functions of this module:
   ! (from top to bottom: "is called by")
   !
-  !               (MODULE DELHOMMEAU_INTEGRALS)
+  !               (Delhommeau_integrals.f90)
   !                          |
   !              COLLECT_DELHOMMEAU_INTEGRALS       (COMPUTE_ASYMPTOTIC_RANKINE_SOURCE)
   !                        /   \                    /
   ! WAVE_PART_INFINITE_DEPTH   WAVE_PART_FINITE_DEPTH
   !                        \   /
-  !                  (build_matrices.f90)
+  !                    (matrices.f90)
   !                          |
   !                    (python code)
 
@@ -26,18 +26,18 @@ CONTAINS
   ! =====================================================================
 
   SUBROUTINE COLLECT_DELHOMMEAU_INTEGRALS                        &
-      (XI, XJ, wavenumber,                                       &
-      tabulated_r_range, tabulated_Z_range, tabulated_integrals, &
+      (X0I, X0J, wavenumber,                                     &
+      tabulated_r_range, tabulated_z_range, tabulated_integrals, &
       FS, VS)
 
     ! Inputs
-    REAL(KIND=PRE), DIMENSION(3),             INTENT(IN) :: XI, XJ
+    REAL(KIND=PRE), DIMENSION(3),             INTENT(IN) :: X0I, X0J
     REAL(KIND=PRE),                           INTENT(IN) :: wavenumber
 
     ! Tabulated data
     REAL(KIND=PRE), DIMENSION(:),             INTENT(IN) :: tabulated_r_range
-    REAL(KIND=PRE), DIMENSION(:),             INTENT(IN) :: tabulated_Z_range
-    REAL(KIND=PRE), DIMENSION(size(tabulated_r_range), size(tabulated_Z_range), 2, 2), INTENT(IN) :: tabulated_integrals
+    REAL(KIND=PRE), DIMENSION(:),             INTENT(IN) :: tabulated_z_range
+    REAL(KIND=PRE), DIMENSION(size(tabulated_r_range), size(tabulated_z_range), 2, 2), INTENT(IN) :: tabulated_integrals
 
     ! Outputs
     COMPLEX(KIND=PRE),                        INTENT(OUT) :: FS  ! the integral
@@ -46,10 +46,10 @@ CONTAINS
     ! Local variables
     REAL(KIND=PRE) :: r, z, r1
     REAL(KIND=PRE), dimension(2, 2) :: integrals
-    REAL(KIND=PRE) :: D1, D2, Z1, Z2
+    COMPLEX(KIND=PRE) :: G, dGdr
 
-    r = wavenumber * NORM2(XI(1:2) - XJ(1:2))
-    z = wavenumber * (XI(3) + XJ(3))
+    r = wavenumber * NORM2(X0I(1:2) - X0J(1:2))
+    z = wavenumber * (X0I(3) + X0J(3))
     r1 = hypot(r, z)
 
     IF (z > -1e-8) THEN
@@ -57,12 +57,12 @@ CONTAINS
       ERROR STOP
     ENDIF
 
-    !=======================================================================
-    ! Evaluate the elementary integrals depending on dimless_Z and dimless_r
-    !=======================================================================
-    IF ((MINVAL(tabulated_Z_range) < z) .AND. (r < MAXVAL(tabulated_r_range))) THEN
+    !=======================================================
+    ! Evaluate the elementary integrals depending on z and r
+    !=======================================================
+    IF ((MINVAL(tabulated_z_range) < z) .AND. (r < MAXVAL(tabulated_r_range))) THEN
         ! Within the range of tabulated data
-        integrals = pick_in_default_tabulation(r, z, tabulated_r_range, tabulated_Z_range, tabulated_integrals)
+        integrals = pick_in_default_tabulation(r, z, tabulated_r_range, tabulated_z_range, tabulated_integrals)
 
       ELSE
         ! Asymptotic expression for distant panels
@@ -73,20 +73,18 @@ CONTAINS
       ! Add the elementary integrals to build FS and VS
       !================================================
 
-      D1 = integrals(1, 1)
-      D2 = integrals(2, 1)
-      Z1 = integrals(1, 2)
-      Z2 = integrals(2, 2)
-
 #ifdef XIE_CORRECTION
-      FS    = CMPLX(Z1/PI + ONE/R1, Z2, KIND=PRE)
-      VS(3) = CMPLX(Z1/PI + ONE/R1, Z2, KIND=PRE)
+      G    = CMPLX(integrals(1, 2)/PI + ONE/r1, integrals(2, 2), KIND=PRE)
+      dGdr = CMPLX(integrals(1, 1)/PI + ONE/r1, integrals(2, 1), KIND=PRE)
 #else
-      FS    = CMPLX(Z1/PI, Z2, KIND=PRE)
-      VS(3) = CMPLX(Z1/PI, Z2, KIND=PRE)
+      G    = CMPLX(integrals(1, 2)/PI, integrals(2, 2), KIND=PRE)
+      dGdr = CMPLX(integrals(1, 1)/PI, integrals(2, 1), KIND=PRE)
 #endif
-      VS(1) = wavenumber*(XJ(1) - XI(1))/r * CMPLX(D1/PI, D2, KIND=PRE)
-      VS(2) = wavenumber*(XJ(2) - XI(2))/r * CMPLX(D1/PI, D2, KIND=PRE)
+
+      FS    = G
+      VS(1) = dGdr * wavenumber*(X0J(1) - X0I(1))/r
+      VS(2) = dGdr * wavenumber*(X0J(2) - X0I(2))/r
+      VS(3) = G
 
       IF (r < REAL(1e-5, KIND=PRE)) THEN
         ! Limit case r ~ 0 ?
@@ -99,8 +97,8 @@ CONTAINS
   ! =========================
 
   SUBROUTINE WAVE_PART_INFINITE_DEPTH &
-      (wavenumber, X0I, X0J,          &
-      X_AXIS, Z_AXIS, TABULATION,     &
+      (X0I, X0J, wavenumber,          &
+      tabulated_r_range, tabulated_z_range, tabulated_integrals, &
       SP, VSP)
     ! Compute the wave part of the Green function in the infinite depth case.
     ! This is mostly the integral computed by the subroutine above.
@@ -111,9 +109,9 @@ CONTAINS
     REAL(KIND=PRE), DIMENSION(3),             INTENT(IN)  :: X0J   ! Coordinates of the center of the integration panel
 
     ! Tabulated data
-    REAL(KIND=PRE), DIMENSION(:),             INTENT(IN) :: X_AXIS
-    REAL(KIND=PRE), DIMENSION(:),             INTENT(IN) :: Z_AXIS
-    REAL(KIND=PRE), DIMENSION(size(X_AXIS), size(Z_AXIS), 2, 2), INTENT(IN) :: TABULATION
+    REAL(KIND=PRE), DIMENSION(:),             INTENT(IN) :: tabulated_r_range
+    REAL(KIND=PRE), DIMENSION(:),             INTENT(IN) :: tabulated_z_range
+    REAL(KIND=PRE), DIMENSION(size(tabulated_r_range), size(tabulated_z_range), 2, 2), INTENT(IN) :: tabulated_integrals
 
     ! Outputs
     COMPLEX(KIND=PRE),               INTENT(OUT) :: SP  ! Integral of the Green function over the panel.
@@ -123,7 +121,10 @@ CONTAINS
     REAL(KIND=PRE), DIMENSION(3) :: XJ_REFLECTION
 
     ! The integrals
-    CALL COLLECT_DELHOMMEAU_INTEGRALS(X0I, X0J, wavenumber, X_AXIS, Z_AXIS, TABULATION, SP, VSP(:))
+    CALL COLLECT_DELHOMMEAU_INTEGRALS(                           &
+      X0I, X0J, wavenumber,                                      &
+      tabulated_r_range, tabulated_z_range, tabulated_integrals, &
+      SP, VSP(:))
     SP  = 2*wavenumber*SP
     VSP = 2*wavenumber**2*VSP
 
@@ -138,8 +139,8 @@ CONTAINS
   ! ======================
 
   SUBROUTINE WAVE_PART_FINITE_DEPTH &
-      (wavenumber, X0I, X0J, depth, &
-      X_AXIS, Z_AXIS, TABULATION,                  &
+      (X0I, X0J, wavenumber, depth, &
+      tabulated_r_range, tabulated_z_range, tabulated_integrals, &
       NEXP, AMBDA, AR,              &
       SP, VSP_SYM, VSP_ANTISYM)
     ! Compute the frequency-dependent part of the Green function in the finite depth case.
@@ -149,10 +150,12 @@ CONTAINS
     REAL(KIND=PRE), DIMENSION(3),             INTENT(IN) :: X0I  ! Coordinates of the source point
     REAL(KIND=PRE), DIMENSION(3),             INTENT(IN) :: X0J  ! Coordinates of the center of the integration panel
 
-    REAL(KIND=PRE), DIMENSION(:),             INTENT(IN) :: X_AXIS
-    REAL(KIND=PRE), DIMENSION(:),             INTENT(IN) :: Z_AXIS
-    REAL(KIND=PRE), DIMENSION(size(X_AXIS), size(Z_AXIS), 2, 2), INTENT(IN) :: TABULATION
+    ! Tabulated data
+    REAL(KIND=PRE), DIMENSION(:),             INTENT(IN) :: tabulated_r_range
+    REAL(KIND=PRE), DIMENSION(:),             INTENT(IN) :: tabulated_z_range
+    REAL(KIND=PRE), DIMENSION(size(tabulated_r_range), size(tabulated_z_range), 2, 2), INTENT(IN) :: tabulated_integrals
 
+    ! Prony decomposition for finite depth
     INTEGER,                                  INTENT(IN) :: NEXP
     REAL(KIND=PRE), DIMENSION(NEXP),          INTENT(IN) :: AMBDA, AR
 
@@ -181,14 +184,20 @@ CONTAINS
     R = NORM2(XI(1:2) - XJ(1:2))
 
     ! 1.a First infinite depth problem
-    CALL COLLECT_DELHOMMEAU_INTEGRALS(XI(:), XJ(:), wavenumber, X_AXIS, Z_AXIS, TABULATION, FS(1), VS(:, 1))
+    CALL COLLECT_DELHOMMEAU_INTEGRALS(                           &
+      XI(:), XJ(:), wavenumber,                                  &
+      tabulated_r_range, tabulated_z_range, tabulated_integrals, &
+      FS(1), VS(:, 1))
 
     PSR(1) = ONE/(wavenumber*SQRT(R**2+(XI(3)+XJ(3))**2))
 
     ! 1.b Shift and reflect XI and compute another value of the Green function
     XI(3) = -X0I(3) - 2*depth
     XJ(3) =  X0J(3)
-    CALL COLLECT_DELHOMMEAU_INTEGRALS(XI(:), XJ(:), wavenumber, X_AXIS, Z_AXIS, TABULATION, FS(2), VS(:, 2))
+    CALL COLLECT_DELHOMMEAU_INTEGRALS(                           &
+      XI(:), XJ(:), wavenumber,                                  &
+      tabulated_r_range, tabulated_z_range, tabulated_integrals, &
+      FS(2), VS(:, 2))
     VS(3, 2) = -VS(3, 2) ! Reflection of the output vector
 
     PSR(2) = ONE/(wavenumber*SQRT(R**2+(XI(3)+XJ(3))**2))
@@ -196,14 +205,20 @@ CONTAINS
     ! 1.c Shift and reflect XJ and compute another value of the Green function
     XI(3) =  X0I(3)
     XJ(3) = -X0J(3) - 2*depth
-    CALL COLLECT_DELHOMMEAU_INTEGRALS(XI(:), XJ(:), wavenumber, X_AXIS, Z_AXIS, TABULATION, FS(3), VS(:, 3))
+    CALL COLLECT_DELHOMMEAU_INTEGRALS(                           &
+      XI(:), XJ(:), wavenumber,                                  &
+      tabulated_r_range, tabulated_z_range, tabulated_integrals, &
+      FS(3), VS(:, 3))
 
     PSR(3) = ONE/(wavenumber*SQRT(R**2+(XI(3)+XJ(3))**2))
 
     ! 1.d Shift and reflect both XI and XJ and compute another value of the Green function
     XI(3) = -X0I(3) - 2*depth
     XJ(3) = -X0J(3) - 2*depth
-    CALL COLLECT_DELHOMMEAU_INTEGRALS(XI(:), XJ(:), wavenumber, X_AXIS, Z_AXIS, TABULATION, FS(4), VS(:, 4))
+    CALL COLLECT_DELHOMMEAU_INTEGRALS(                           &
+      XI(:), XJ(:), wavenumber,                                  &
+      tabulated_r_range, tabulated_z_range, tabulated_integrals, &
+      FS(4), VS(:, 4))
     VS(3, 4) = -VS(3, 4) ! Reflection of the output vector
 
     PSR(4) = ONE/(wavenumber*SQRT(R**2+(XI(3)+XJ(3))**2))

pytest/test_bem_green_functions.py
@@ -104,11 +104,11 @@ gravity = 9.8
 def wave_part_Green_function(Xi, Xj, omega, depth, method):
     if depth == np.infty:
         wavenumber = omega**2 / gravity
-        return method.fortran_core.green_wave.wave_part_infinite_depth(wavenumber, Xi, Xj, method.tabulated_r_range, method.tabulated_z_range, method.tabulated_integrals)
+        return method.fortran_core.green_wave.wave_part_infinite_depth(Xi, Xj, wavenumber, method.tabulated_r_range, method.tabulated_z_range, method.tabulated_integrals)
     else:
         wavenumber = newton(lambda x: x*np.tanh(x) - omega**2*depth/gravity, x0=1.0)/depth
         ambda, ar, nexp = method.fortran_core.old_prony_decomposition.lisc(omega**2 * depth/gravity, wavenumber * depth)
-        return method.fortran_core.green_wave.wave_part_finite_depth(wavenumber, Xi, Xj, depth, method.tabulated_r_range, method.tabulated_z_range, method.tabulated_integrals, ambda, ar, 31)
+        return method.fortran_core.green_wave.wave_part_finite_depth(Xi, Xj, wavenumber, depth, method.tabulated_r_range, method.tabulated_z_range, method.tabulated_integrals, ambda, ar, 31)
 
 
 @given(points, points, frequencies, depths, methods)

capytaine/green_functions/Delhommeau_f90/Delhommeau_integrals.f90
@@ -73,11 +73,7 @@ contains
       else
         exp_zeta = exp(zeta)
       endif
-      if (aimag(zeta) < 0) then
-        jzeta = exp_e1(zeta) - ii*pi*exp_zeta
-      else
-        jzeta = exp_e1(zeta) + ii*pi*exp_zeta
-      end if
+      jzeta = exp_e1(zeta) + ii*pi*exp_zeta
       integrals(1, 1) = integrals(1, 1) + delta_theta * cos_theta * aimag(jzeta - 1.0/zeta)
       integrals(2, 1) = integrals(2, 1) + delta_theta * cos_theta * aimag(exp_zeta)
 #ifdef XIE_CORRECTION

capytaine/green_functions/Delhommeau_f90/Green_Rankine.f90
@@ -11,7 +11,7 @@ MODULE GREEN_RANKINE
 
 CONTAINS
 
-  SUBROUTINE COMPUTE_INTEGRAL_OF_RANKINE_SOURCE                     &
+  PURE SUBROUTINE COMPUTE_INTEGRAL_OF_RANKINE_SOURCE                &
       (M,                                                           &
       Face_nodes, Face_center, Face_normal, Face_area, Face_radius, &
       S0, VS0)
@@ -50,7 +50,7 @@ CONTAINS
 
       GZ = DOT_PRODUCT(M(1:3) - Face_center(1:3), Face_normal(1:3)) ! Called Z in [Del]
 
-      DO L = 1, 4
+      DO CONCURRENT (L = 1:4)
         RR(L) = NORM2(M(1:3) - Face_nodes(L, 1:3))       ! Distance from vertices of Face to M.
         DRX(:, L) = (M(1:3) - Face_nodes(L, 1:3))/RR(L)  ! Normed vector from vertices of Face to M.
       END DO
@@ -108,7 +108,7 @@ CONTAINS
 
   ! =========================
 
-  SUBROUTINE COMPUTE_ASYMPTOTIC_RANKINE_SOURCE &
+  PURE SUBROUTINE COMPUTE_ASYMPTOTIC_RANKINE_SOURCE &
       (M,                                      &
       Face_center, Face_area,                  &
       S0, VS0)

capytaine/green_functions/Delhommeau_f90/constants.f90
@@ -3,11 +3,12 @@
 
 MODULE CONSTANTS
 
+  IMPLICIT NONE
+
   INTEGER, PARAMETER :: PRE = KIND(1d0) ! Floating point numbers precision
 
   REAL(KIND=PRE), PARAMETER :: ZERO = 0
   REAL(KIND=PRE), PARAMETER :: ONE = 1
-  REAL(KIND=PRE), PARAMETER :: INFINITE_DEPTH = 0
 
   REAL(KIND=PRE), PARAMETER :: PI = 3.141592653588979 ! π
   COMPLEX(KIND=PRE), PARAMETER :: II = (0, 1)         ! Imaginary unit

capytaine/green_functions/Delhommeau_f90/matrices.f90
@@ -1,5 +1,6 @@
 MODULE MATRICES
 
+  USE ieee_arithmetic
   USE CONSTANTS
 
   USE GREEN_RANKINE
@@ -8,6 +9,15 @@ MODULE MATRICES
   IMPLICIT NONE
 
 CONTAINS
+
+  ! =====================================================================
+
+  PURE LOGICAL FUNCTION is_infinity(x)
+    REAL(KIND=PRE), INTENT(IN) :: x
+    is_infinity = (.NOT. ieee_is_finite(x))
+  END FUNCTION
+
+  ! =====================================================================
 
   SUBROUTINE ADD_RANKINE_PART_TO_THE_MATRICES(                        &
       nb_faces_1,                                                     &
@@ -65,7 +75,7 @@ CONTAINS
       nb_faces_2, nb_quad_points,        &
       quad_points, quad_weights,         &
       wavenumber, depth,                 &
-      XR, XZ, APD,                       &
+      tabulated_r_range, tabulated_z_range, tabulated_integrals, &
       NEXP, AMBDA, AR,                   &
       coeff,                             &
       same_body,                         &
@@ -81,10 +91,10 @@ CONTAINS
 
     REAL(KIND=PRE),                           INTENT(IN) :: wavenumber, depth
 
-    ! Tabulated integrals
-    REAL(KIND=PRE), DIMENSION(:),             INTENT(IN) :: XR
-    REAL(KIND=PRE), DIMENSION(:),             INTENT(IN) :: XZ
-    REAL(KIND=PRE), DIMENSION(size(XR), size(XZ), 2, 2), INTENT(IN) :: APD
+    ! Tabulated data
+    REAL(KIND=PRE), DIMENSION(:),             INTENT(IN) :: tabulated_r_range
+    REAL(KIND=PRE), DIMENSION(:),             INTENT(IN) :: tabulated_z_range
+    REAL(KIND=PRE), DIMENSION(size(tabulated_r_range), size(tabulated_z_range), 2, 2), INTENT(IN) :: tabulated_integrals
 
     ! Prony decomposition for finite depth
     INTEGER,                                  INTENT(IN) :: NEXP
@@ -114,22 +124,22 @@ CONTAINS
         !$OMP PARALLEL DO PRIVATE(J, SP2, VSP2_SYM, VSP2_ANTISYM)
         DO J = I, nb_faces_2
 
-          IF (depth == INFINITE_DEPTH) THEN
+          IF (is_infinity(depth)) THEN
             CALL WAVE_PART_INFINITE_DEPTH &
-              (wavenumber,                &
-              centers_1(I, :),            &
+              (centers_1(I, :),           &
               quad_points(J, 1, :),       & ! centers_2(J, :),
-              XR, XZ, APD,                &
+              wavenumber,                 &
+              tabulated_r_range, tabulated_z_range, tabulated_integrals, &
               SP2, VSP2_SYM               &
               )
             VSP2_ANTISYM(:) = ZERO
           ELSE
             CALL WAVE_PART_FINITE_DEPTH   &
-              (wavenumber,                &
-              centers_1(I, :),            &
+              (centers_1(I, :),           &
               quad_points(J, 1, :),       & ! centers_2(J, :),
+              wavenumber,                 &
               depth,                      &
-              XR, XZ, APD,                &
+              tabulated_r_range, tabulated_z_range, tabulated_integrals, &
               NEXP, AMBDA, AR,            &
               SP2, VSP2_SYM, VSP2_ANTISYM &
               )
@@ -157,22 +167,22 @@ CONTAINS
         !$OMP PARALLEL DO PRIVATE(J, SP2, VSP2_SYM, VSP2_ANTISYM)
         DO J = 1, nb_faces_2
           DO Q = 1, nb_quad_points
-            IF (depth == INFINITE_DEPTH) THEN
+            IF (is_infinity(depth)) THEN
               CALL WAVE_PART_INFINITE_DEPTH &
-                (wavenumber,                &
-                centers_1(I, :),            &
-                quad_points(J, Q, :),       &
-                XR, XZ, APD,                &
+                (centers_1(I, :),           &
+                quad_points(J, Q, :),       & ! centers_2(J, :),
+                wavenumber,                 &
+                tabulated_r_range, tabulated_z_range, tabulated_integrals, &
                 SP2, VSP2_SYM               &
                 )
               VSP2_ANTISYM(:) = ZERO
             ELSE
               CALL WAVE_PART_FINITE_DEPTH   &
-                (wavenumber,                &
-                centers_1(I, :),            &
-                quad_points(J, Q, :),       &
+                (centers_1(I, :),           &
+                quad_points(J, Q, :),       & ! centers_2(J, :),
+                wavenumber,                 &
                 depth,                      &
-                XR, XZ, APD,                &
+                tabulated_r_range, tabulated_z_range, tabulated_integrals, &
                 NEXP, AMBDA, AR,            &
                 SP2, VSP2_SYM, VSP2_ANTISYM &
                 )
@@ -199,7 +209,7 @@ CONTAINS
       nb_quad_points, quad_points, quad_weights,      &
       wavenumber, depth,                              &
       coeffs,                                         &
-      XR, XZ, APD,                                    &
+      tabulated_r_range, tabulated_z_range, tabulated_integrals, &
       NEXP, AMBDA, AR,                                &
       same_body,                                      &
       S, K)
@@ -222,10 +232,10 @@ CONTAINS
 
     REAL(KIND=PRE), DIMENSION(3) :: coeffs
 
-    ! Tabulated integrals
-    REAL(KIND=PRE), DIMENSION(:),             INTENT(IN) :: XR
-    REAL(KIND=PRE), DIMENSION(:),             INTENT(IN) :: XZ
-    REAL(KIND=PRE), DIMENSION(size(XR), size(XZ), 2, 2), INTENT(IN) :: APD
+    ! Tabulated data
+    REAL(KIND=PRE), DIMENSION(:),             INTENT(IN) :: tabulated_r_range
+    REAL(KIND=PRE), DIMENSION(:),             INTENT(IN) :: tabulated_z_range
+    REAL(KIND=PRE), DIMENSION(size(tabulated_r_range), size(tabulated_z_range), 2, 2), INTENT(IN) :: tabulated_integrals
 
     ! Prony decomposition for finite depth
     INTEGER,                                  INTENT(IN) :: NEXP
@@ -269,7 +279,7 @@ CONTAINS
 
     IF (coeffs(2) .NE. ZERO) THEN
 
-      IF (depth == INFINITE_DEPTH) THEN
+      IF (is_infinity(depth)) THEN
         ! Reflection through free surface
         reflected_centers_1(:, 1:2) = centers_1(:, 1:2)
         reflected_centers_1(:, 3)   = -centers_1(:, 3)
@@ -303,7 +313,7 @@ CONTAINS
         nb_faces_2, nb_quad_points,        &
         quad_points, quad_weights,         &
         wavenumber, depth,                 &
-        XR, XZ, APD,                       &
+        tabulated_r_range, tabulated_z_range, tabulated_integrals, &
         NEXP, AMBDA, AR,                   &
         coeffs(3),                         &
         same_body,                         &

capytaine/green_functions/delhommeau.py
@@ -203,7 +203,7 @@ class Delhommeau(AbstractGreenFunction):
             mesh2.faces_centers, mesh2.faces_normals,
             mesh2.faces_areas,   mesh2.faces_radiuses,
             *mesh2.quadrature_points,
-            wavenumber, 0.0 if depth == np.infty else depth,
+            wavenumber, depth,
             coeffs,
             self.tabulated_r_range, self.tabulated_z_range, self.tabulated_integrals,
             lamda_exp, a_exp,

commit 588c32ab828239e30eb66e9af99ff3feede4e975

Fix bug introduced in 39df584
The XieDelhommeau Green function was wrongly reformulated in PR #158.

capytaine/green_functions/Delhommeau_f90/Green_wave.f90
@@ -75,11 +75,10 @@ CONTAINS
 
 #ifdef XIE_CORRECTION
       G    = CMPLX(integrals(1, 2)/PI + ONE/r1, integrals(2, 2), KIND=PRE)
-      dGdr = CMPLX(integrals(1, 1)/PI + ONE/r1, integrals(2, 1), KIND=PRE)
 #else
       G    = CMPLX(integrals(1, 2)/PI, integrals(2, 2), KIND=PRE)
-      dGdr = CMPLX(integrals(1, 1)/PI, integrals(2, 1), KIND=PRE)
 #endif
+      dGdr = CMPLX(integrals(1, 1)/PI, integrals(2, 1), KIND=PRE)
 
       FS    = G
       VS(1) = dGdr * wavenumber*(X0J(1) - X0I(1))/r

pytest/test_bem_green_functions.py
@@ -91,7 +91,6 @@ def test_compare_tabulations_of_Delhommeau_and_XieDelhommeau():
     Xie = gfs[1].tabulated_integrals[:, :, 0, 1]
     assert np.allclose(Del[abs(z) > 1], Xie[abs(z) > 1], atol=1e-3)
 
-
 points = arrays(float, (3,),
                 elements=floats(min_value=-10.0, max_value=10.0, allow_infinity=False, allow_nan=False)
                 ).filter(lambda x: x[2] < -1e-2)
@@ -126,4 +125,12 @@ def test_symmetry_of_the_derivative_of_the_Green_function(X1, X2, omega, method)
                       wave_part_Green_function(X2, X1, omega, np.infty, method)[1][2],
                       rtol=1e-4)
 
+
+@given(points, points, frequencies)
+def test_compare_output_of_Delhommeau_and_XieDelhommeau(X1, X2, omega):
+    # Unchanged parts of the Green function:
+    assert np.allclose(
+            wave_part_Green_function(X1, X2, omega, np.infty, gfs[0])[1][0:2],
+            wave_part_Green_function(X1, X2, omega, np.infty, gfs[1])[1][0:2],
+            rtol=1e-5)
 