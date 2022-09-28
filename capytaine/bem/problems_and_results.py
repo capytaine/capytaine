08dcb510ec8adbc0b04530c6c74d81adf8204251
Matthieu Ancellin <matthieu.ancellin@eurobios.com>
2022-09-28 14:40:33 +0200

    Reword warning.

--- capytaine/bem/problems_and_results.py
+++ capytaine/bem/problems_and_results.py
@@ -99,8 +99,11 @@ class LinearPotentialFlowProblem:
                 )
 
             if self.wavelength < 8*self.body.mesh.faces_radiuses.max():
-                LOG.warning(f"Resolution of the mesh (8×max_radius={8*self.body.mesh.faces_radiuses.max():.2e}) "
-                            f"might be insufficient for this wavelength (wavelength={self.wavelength:.2e})!")
+                LOG.warning(f"Mesh resolution for {self}:\n"
+                        f"The resolution of the mesh '{self.body.mesh.name}' of the body '{self.body.name}' "
+                        f"might be insufficient for the wavelength λ={self.wavelength:.2e}.\n"
+                        f"This warning appears because the largest panel of this mesh has radius {self.body.mesh.faces_radiuses.max():.2e} > λ/8."
+                        )
 
         if self.boundary_condition is not None:
             if len(self.boundary_condition.shape) != 1:
