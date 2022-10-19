f7f94d3ab285e5efe5b5be5c673c093cee89860f
Matthieu Ancellin <matthieu.ancellin@eurobios.com>
2022-10-19 16:03:42 +0200

    Fix merge issue.

--- pytest/test_bem_green_functions.py
+++ pytest/test_bem_green_functions.py
@@ -125,8 +125,8 @@ def test_symmetry_of_the_derivative_of_the_Green_function(X1, X2, omega, method)
 
 
 def test_floating_point_precision():
-    assert Delhommeau(floating_point_precision="float64").tabulated_integrals.dtype == np.float64
-    assert Delhommeau(floating_point_precision="float32").tabulated_integrals.dtype == np.float32
+    assert cpt.Delhommeau(floating_point_precision="float64").tabulated_integrals.dtype == np.float64
+    assert cpt.Delhommeau(floating_point_precision="float32").tabulated_integrals.dtype == np.float32
 
 
 def test_no_tabulation():
