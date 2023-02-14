600900e8733f203b3e68d73f6fcc35dc74ba7073
Matthieu Ancellin <matthieu.ancellin@eurobios.com>
2023-02-14 11:13:51 +0100

    Add another plot in test.
    To be moved to examples?

--- pytest/test_bem_velocity_reconstruction.py
+++ pytest/test_bem_velocity_reconstruction.py
@@ -54,12 +54,33 @@ def test_solver_velocity_reconstruction():
     mesh = cpt.mesh_horizontal_cylinder(radius=1.0, length=1.0, center=(0, 0, 0), resolution=(5, 20, 5)).immersed_part()
     body = cpt.FloatingBody(mesh=mesh, dofs=cpt.rigid_body_dofs())
     velocities = {}
+    solver = cpt.BEMSolver()
     for dof in body.dofs:
         pb = cpt.RadiationProblem(body=body, omega=1.0, radiating_dof=dof)
-        solver = cpt.BEMSolver()
         res = solver.solve(pb)
-        print(solver.get_velocity_at_points(res, np.array([[0.0, 0.0, -1.0001]])))
         velocities[dof] = solver.get_velocity_on_mesh(res, mesh)
+
+    Y, Z = np.meshgrid(np.linspace(-3.0, 3.0, 40), np.linspace(-3.0, -0.01, 20))
+    points = np.vstack([np.zeros(Y.size), Y.ravel(), Z.ravel()]).T
+    velocities = {}
+    for dof in body.dofs:
+        pb = cpt.RadiationProblem(body=body, omega=1.0, radiating_dof=dof)
+        res = solver.solve(pb)
+        velocities[dof] = solver.get_velocity_at_points(res, points)
+    import matplotlib.pyplot as plt
+    fig, axs = plt.subplots(1, 2, figsize=(12, 4))
+    for ax, dof in zip(axs.ravel(), ["Sway", "Heave"]):
+        y_velocities = -velocities[dof][:, 1].reshape(Y.shape).real
+        z_velocities = -velocities[dof][:, 2].reshape(Z.shape).real
+        ax.quiver(Y, Z, y_velocities, z_velocities, angles="xy")
+        ax.axis('equal')
+        theta = np.linspace(-np.pi, 0.0, 100)
+        ax.plot(np.cos(theta), np.sin(theta), color="red")
+        ax.set_title(dof)
+        ax.set_xlabel("y")
+        ax.set_ylabel("z")
+    plt.show()
+
 
     # import matplotlib.pyplot as plt
     # fig, axs = plt.subplots(2, 3, subplot_kw=dict(projection="3d"))
