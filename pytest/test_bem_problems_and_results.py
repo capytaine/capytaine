capytaine/capytaine

commit fca466fc1ddc9e6dde1b4031f47008dd92a6cce4

Allow user to set period, wavelength or wavenumber. (#283)

capytaine/io/xarray.py
@@ -11,6 +11,7 @@
 import logging
 from datetime import datetime
 from itertools import product
+from collections import Counter
 from typing import Sequence, List, Union
 
 import numpy as np
@@ -65,7 +66,9 @@ def problems_from_dataset(dataset: xr.Dataset,
 
     # Warn user in case of key with unrecognized name (e.g. mispells)
     keys_in_dataset = set(dataset.keys()) | set(dataset.coords.keys())
-    accepted_keys = {'wave_direction', 'radiating_dof', 'body_name', 'omega', 'water_depth', 'rho', 'g'}
+    accepted_keys = {'wave_direction', 'radiating_dof', 'body_name',
+                     'omega', 'period', 'wavelength', 'wavenumber',
+                     'water_depth', 'rho', 'g'}
     unrecognized_keys = keys_in_dataset.difference(accepted_keys)
     if len(unrecognized_keys) > 0:
         LOG.warning(f"Unrecognized key(s) in dataset: {unrecognized_keys}")
@@ -73,11 +76,22 @@ def problems_from_dataset(dataset: xr.Dataset,
     if ("radiating_dof" not in keys_in_dataset) and ("wave_direction" not in keys_in_dataset):
         raise ValueError("Neither 'radiating_dof' nor 'wave_direction' has been provided in the dataset. "
                 "No linear potential flow problem can be inferred.")
+
+    frequency_keys = keys_in_dataset & {'omega', 'period', 'wavelength', 'wavenumber'}
+    if len(frequency_keys) > 1:
+            raise ValueError("Setting problems requires at most one of the following: omega (angular frequency) OR period OR wavenumber OR wavelength.\n"
+                             "Received {}".format(frequency_keys))
     # END SANITY CHECKS
 
     dataset = _unsqueeze_dimensions(dataset)
 
-    omega_range = dataset['omega'].data if 'omega' in dataset else [_default_parameters['omega']]
+    if len(frequency_keys) == 0:
+        freq_type = "omega"
+        freq_range = [_default_parameters['omega']]
+    else:  # len(frequency_keys) == 1
+        freq_type = list(frequency_keys)[0]  # Get the only item
+        freq_range = dataset[freq_type].data
+
     water_depth_range = dataset['water_depth'].data if 'water_depth' in dataset else [_default_parameters['water_depth']]
     rho_range = dataset['rho'].data if 'rho' in dataset else [_default_parameters['rho']]
     g_range = dataset['g'].data if 'g' in dataset else [_default_parameters['g']]
@@ -96,18 +110,18 @@ def problems_from_dataset(dataset: xr.Dataset,
 
     problems = []
     if wave_direction_range is not None:
-        for omega, wave_direction, water_depth, body_name, rho, g \
-                in product(omega_range, wave_direction_range, water_depth_range, body_range, rho_range, g_range):
+        for freq, wave_direction, water_depth, body_name, rho, g \
+                in product(freq_range, wave_direction_range, water_depth_range, body_range, rho_range, g_range):
             problems.append(
-                DiffractionProblem(body=body_range[body_name], omega=omega,
+                DiffractionProblem(body=body_range[body_name], **{freq_type: freq},
                                    wave_direction=wave_direction, sea_bottom=-water_depth, rho=rho, g=g)
             )
 
     if radiating_dofs is not None:
-        for omega, radiating_dof, water_depth, body_name, rho, g \
-                in product(omega_range, radiating_dofs, water_depth_range, body_range, rho_range, g_range):
+        for freq, radiating_dof, water_depth, body_name, rho, g \
+                in product(freq_range, radiating_dofs, water_depth_range, body_range, rho_range, g_range):
             problems.append(
-                RadiationProblem(body=body_range[body_name], omega=omega,
+                RadiationProblem(body=body_range[body_name], **{freq_type: freq},
                                  radiating_dof=radiating_dof, sea_bottom=-water_depth, rho=rho, g=g)
             )
 
@@ -254,7 +268,7 @@ def collect_records(results):
     return records_list
 
 def assemble_dataset(results,
-                     wavenumber=False, wavelength=False, mesh=False, hydrostatics=True,
+                     wavenumber=False, wavelength=False, period=False, mesh=False, hydrostatics=True,
                      attrs=None) -> xr.Dataset:
     """Transform a list of :class:`LinearPotentialFlowResult` into a :class:`xarray.Dataset`.
 
@@ -270,6 +284,8 @@ def assemble_dataset(results,
         If True, the coordinate 'wavenumber' will be added to the output dataset.
     wavelength: bool, optional
         If True, the coordinate 'wavelength' will be added to the output dataset.
+    period: bool, optional
+        If True, the coordinate 'period' will be added to the output dataset.
     mesh: bool, optional
         If True, store some infos on the mesh in the output dataset.
     hydrostatics: bool, optional
@@ -348,7 +364,7 @@ def assemble_dataset(results,
         dataset = xr.merge([dataset, diffraction_cases])
 
     # WAVENUMBER
-    if wavenumber:
+    if wavenumber or any(res.problem.provided_freq_type == "wavenumber" for res in results):
         if bemio_import:
             wavenumber_ds = _dataset_from_dataframe(
                 records.drop_duplicates(subset=['omega']),
@@ -360,7 +376,7 @@ def assemble_dataset(results,
             dataset.coords['wavenumber'] = wavenumber_data_array(results)
         dataset.wavenumber.attrs['long_name'] = 'Wave number'
 
-    if wavelength:
+    if wavelength or any(res.problem.provided_freq_type == "wavelength" for res in results):
         if bemio_import:
             wavelength_ds = _dataset_from_dataframe(
                     records.drop_duplicates(subset=['omega']),
@@ -371,6 +387,18 @@ def assemble_dataset(results,
         else:
             dataset.coords['wavelength'] = 2*np.pi/wavenumber_data_array(results)
         dataset.wavelength.attrs['long_name'] = 'Wave length'
+
+    if period or any(res.problem.provided_freq_type == "period" for res in results):
+        if bemio_import:
+            period_ds = _dataset_from_dataframe(
+                    records.drop_duplicates(subset=['omega']),
+                    variables=['period'],
+                    dimensions=['omega'],
+                    optional_dims=['g', 'water_depth'])
+            dataset.coords['period'] = wavelength_ds['period']
+        else:
+            dataset.coords['period'] = 2*np.pi/dataset["omega"]
+        dataset.wavelength.attrs['long_name'] = 'Period'
 
     if mesh:
         if bemio_import:
@@ -406,6 +434,9 @@ def assemble_dataset(results,
     dataset.attrs['capytaine_version'] = __version__
     dataset.omega.attrs['long_name'] = 'Radial frequency'
     dataset.omega.attrs['units'] = 'rad/s'
+
+    main_freq_type = Counter((res.problem.provided_freq_type for res in results)).most_common(1)[0][0]
+    dataset = dataset.swap_dims({"omega": main_freq_type})
     return dataset
 
 

pytest/test_bem_problems_and_results.py
@@ -9,6 +9,8 @@ import pytest
 import numpy as np
 import xarray as xr
 
+import capytaine as cpt
+
 from capytaine.meshes.meshes import Mesh
 from capytaine.meshes.symmetric import ReflectionSymmetricMesh
 
@@ -23,7 +25,7 @@ from capytaine.io.xarray import problems_from_dataset, assemble_dataset
 
 from capytaine.io.legacy import import_cal_file
 
-solver = Nemoh(matrix_cache_size=0)
+solver = cpt.BEMSolver()
 
 
 def test_LinearPotentialFlowProblem():
@@ -80,19 +82,39 @@ def test_LinearPotentialFlowProblem():
     assert res.body is pb.body
 
 
+@pytest.mark.parametrize("water_depth", [10.0, np.infty])
+def test_setting_wavelength(water_depth):
+    λ = 10*np.random.rand()
+    assert np.isclose(cpt.DiffractionProblem(wavelength=λ, sea_bottom=-water_depth).wavelength, λ)
+
+@pytest.mark.parametrize("water_depth", [10.0, np.infty])
+def test_setting_wavenumber(water_depth):
+    k = 10*np.random.rand()
+    assert np.isclose(cpt.DiffractionProblem(wavenumber=k, sea_bottom=-water_depth).wavenumber, k)
+
+@pytest.mark.parametrize("water_depth", [10.0, np.infty])
+def test_setting_period(water_depth):
+    T = 10*np.random.rand()
+    assert np.isclose(cpt.DiffractionProblem(period=T, sea_bottom=-water_depth).period, T)
+
+def test_setting_too_many_frequencies():
+    with pytest.raises(ValueError, match="at most one"):
+        cpt.DiffractionProblem(omega=1.0, wavelength=1.0)
+
+
 def test_diffraction_problem():
-    assert DiffractionProblem().body is None
+    assert DiffractionProblem(omega=1.0).body is None
 
     sphere = Sphere(radius=1.0, ntheta=20, nphi=40)
     sphere.add_translation_dof(direction=(0, 0, 1), name="Heave")
 
-    pb = DiffractionProblem(body=sphere, wave_direction=1.0)
+    pb = DiffractionProblem(body=sphere, wave_direction=1.0, wavenumber=1.0)
     assert len(pb.boundary_condition) == sphere.mesh.nb_faces
 
     with pytest.raises(TypeError):
         DiffractionProblem(boundary_conditions=[0, 0, 0])
 
-    assert "DiffractionProblem" in str(DiffractionProblem(g=10, rho=1025, free_surface=np.infty))
+    assert "DiffractionProblem" in str(DiffractionProblem(wavenumber=1.0, g=10, rho=1025, free_surface=np.infty))
 
     res = pb.make_results_container()
     assert isinstance(res, DiffractionResult)
@@ -234,6 +256,23 @@ def test_problems_from_dataset():
     assert len(problems) == 12
 
 
+def test_problems_from_dataset_with_wavelength():
+    body = cpt.FloatingBody(mesh=cpt.mesh_sphere(center=(0, 0, -4), name="sphere"),
+                            dofs=cpt.rigid_body_dofs(rotation_center=(0, 0, -4)))
+    dset = xr.Dataset(coords={'wavelength': [12.0], 'radiating_dof': ["Heave"]})
+    problems = problems_from_dataset(dset, body)
+    for pb in problems:
+        assert np.isclose(pb.wavelength, 12.0)
+
+
+def test_problems_from_dataset_with_too_many_info():
+    body = cpt.FloatingBody(mesh=cpt.mesh_sphere(center=(0, 0, -4), name="sphere"),
+                            dofs=cpt.rigid_body_dofs(rotation_center=(0, 0, -4)))
+    dset = xr.Dataset(coords={'wavelength': [12.0], 'period': [3.0], 'radiating_dof': ["Heave"]})
+    with pytest.raises(ValueError, match="at most one"):
+        problems = problems_from_dataset(dset, body)
+
+
 def test_assemble_dataset():
     body = Sphere(center=(0, 0, -4), name="sphere")
     body.add_translation_dof(name="Heave")
@@ -260,3 +299,13 @@ def test_fill_dataset():
     dataset = solver.fill_dataset(test_matrix, [body])
     assert dataset['added_mass'].data.shape == (3, 1, 6)
     assert dataset['Froude_Krylov_force'].data.shape == (3, 2, 6)
+
+
+def test_fill_dataset_with_wavenumbers():
+    body = cpt.FloatingBody(mesh=cpt.mesh_horizontal_cylinder(radius=1, center=(0, 0, -2)),
+                            dofs=cpt.rigid_body_dofs(rotation_center=(0, 0, -2)))
+    k_range = np.linspace(1.0, 3.0, 3)
+    test_matrix = xr.Dataset(coords={'wavenumber': k_range, 'wave_direction': [0, np.pi/2], 'radiating_dof': ['Heave']})
+    dataset = solver.fill_dataset(test_matrix, [body])
+    assert np.allclose(dataset.coords['wavenumber'], k_range)
+    print(dataset)

capytaine/bem/problems_and_results.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
 """Definition of the problems to solve with the BEM solver, and the results of this resolution."""
-# Copyright (C) 2017-2020 Matthieu Ancellin
-# See LICENSE file at <https://github.com/mancellin/capytaine>
+# Copyright (C) 2017-2023 Matthieu Ancellin
+# See LICENSE file at <https://github.com/capytaine/capytaine>
 
 import logging
 
@@ -23,6 +23,9 @@ _default_parameters = {'rho': 1000.0, 'g': 9.81, 'omega': 1.0,
 class LinearPotentialFlowProblem:
     """General class of a potential flow problem.
 
+    At most one of the following parameter must be provided: omega, period, wavenumber or wavelength.
+    Internally only omega is stored, hence setting another parameter can lead to small rounding errors.
+
     Parameters
     ----------
     body: FloatingBody, optional
@@ -32,12 +35,18 @@ class LinearPotentialFlowProblem:
     sea_bottom: float, optional
         The position of the sea bottom
     omega: float, optional
-        The frequency of the waves in rad/s
+        The angular frequency of the waves in rad/s
+    period: float, optional
+        The period of the waves in s
+    wavenumber: float, optional
+        The angular wave number of the waves in rad/m
+    wavelength: float, optional
+        The wave length of the waves in m
     rho: float, optional
         The density of water in kg/m3 (default: 1000.0)
     g: float, optional
         The acceleration of gravity in m/s2 (default: 9.81)
-    boundary_condition: np.ndarray of shape (body.mesh.nb_faces,)
+    boundary_condition: np.ndarray of shape (body.mesh.nb_faces,), optional
         The Neumann boundary condition on the floating body
 
     TODO: more consistent use of free_surface and sea_bottom vs. water_depth
@@ -47,7 +56,7 @@ class LinearPotentialFlowProblem:
                  body=None,
                  free_surface=_default_parameters['free_surface'],
                  sea_bottom=-_default_parameters['water_depth'],
-                 omega=_default_parameters['omega'],
+                 omega=None, period=None, wavenumber=None, wavelength=None,
                  rho=_default_parameters['rho'],
                  g=_default_parameters['g'],
                  boundary_condition=None):
@@ -55,12 +64,34 @@ class LinearPotentialFlowProblem:
         self.body = body
         self.free_surface = float(free_surface)
         self.sea_bottom = float(sea_bottom)
-        self.omega = float(omega)
         self.rho = float(rho)
         self.g = float(g)
         self.boundary_condition = boundary_condition
 
+        self.omega, self.provided_freq_type = self._get_angular_frequency(omega, period, wavenumber, wavelength)
+
         self._check_data()
+
+
+    def _get_angular_frequency(self, omega, period, wavenumber, wavelength):
+        frequency_data = dict(omega=omega, period=period, wavenumber=wavenumber, wavelength=wavelength)
+        nb_provided_frequency_data = 4 - list(frequency_data.values()).count(None)
+
+        if nb_provided_frequency_data > 1:
+            raise ValueError("Settings a problem requires at most one of the following: omega (angular frequency) OR period OR wavenumber OR wavelength.\n"
+                             "Received {} of them: {}".format(nb_provided_frequency_data, {k: v for k, v in frequency_data.items() if v is not None}))
+
+        if omega is not None:
+            return float(omega), "omega"
+        elif period is not None:
+            return 2*np.pi/period, "period"
+        elif wavenumber is not None:
+            return np.sqrt(self.g*wavenumber*np.tanh(wavenumber*self.depth)), "wavenumber"
+        elif wavelength is not None:
+            return np.sqrt(self.g*2*np.pi/wavelength*np.tanh(2*np.pi/wavelength*self.depth)), "wavelength"
+        else:
+            return _default_parameters["omega"], "omega"
+
 
     def _check_data(self):
         """Sanity checks on the data."""
@@ -234,7 +265,7 @@ class DiffractionProblem(LinearPotentialFlowProblem):
                  body=None,
                  free_surface=_default_parameters['free_surface'],
                  sea_bottom=-_default_parameters['water_depth'],
-                 omega=_default_parameters['omega'],
+                 omega=None, period=None, wavenumber=None, wavelength=None,
                  rho=_default_parameters['rho'],
                  g=_default_parameters['g'],
                  wave_direction=_default_parameters['wave_direction']):
@@ -242,7 +273,7 @@ class DiffractionProblem(LinearPotentialFlowProblem):
         self.wave_direction = float(wave_direction)
 
         super().__init__(body=body, free_surface=free_surface, sea_bottom=sea_bottom,
-                         omega=omega, rho=rho, g=g)
+                         omega=omega, period=period, wavenumber=wavenumber, wavelength=wavelength, rho=rho, g=g)
 
         if not (-2*np.pi-1e-3 <= self.wave_direction <= 2*np.pi+1e-3):
             LOG.warning(f"The value {self.wave_direction} has been provided for the wave direction, and it does not look like an angle in radians. "
@@ -281,7 +312,7 @@ class RadiationProblem(LinearPotentialFlowProblem):
     def __init__(self, *, body=None,
                  free_surface=_default_parameters['free_surface'],
                  sea_bottom=-_default_parameters['water_depth'],
-                 omega=_default_parameters['omega'],
+                 omega=None, period=None, wavenumber=None, wavelength=None,
                  rho=_default_parameters['rho'],
                  g=_default_parameters['g'],
                  radiating_dof=None):
@@ -289,7 +320,7 @@ class RadiationProblem(LinearPotentialFlowProblem):
         self.radiating_dof = radiating_dof
 
         super().__init__(body=body, free_surface=free_surface, sea_bottom=sea_bottom,
-                         omega=omega, rho=rho, g=g)
+                         omega=omega, period=period, wavenumber=wavenumber, wavelength=wavelength, rho=rho, g=g)
 
         if self.body is not None:
 

commit 5e37ace5cda31ce7d458ab9e80e8243a9fed9af6

Fix bug introduced by #283

capytaine/io/xarray.py
@@ -400,7 +400,7 @@ def assemble_dataset(results,
             dataset.coords['period'] = wavelength_ds['period']
         else:
             dataset.coords['period'] = 2*np.pi/dataset["omega"]
-        dataset.wavelength.attrs['long_name'] = 'Period'
+        dataset.period.attrs['long_name'] = 'Period'
 
     if mesh:
         if bemio_import:

pytest/test_bem_problems_and_results.py
@@ -307,4 +307,12 @@ def test_fill_dataset_with_wavenumbers():
     test_matrix = xr.Dataset(coords={'wavenumber': k_range, 'wave_direction': [0, np.pi/2], 'radiating_dof': ['Heave']})
     dataset = solver.fill_dataset(test_matrix, [body])
     assert np.allclose(dataset.coords['wavenumber'], k_range)
-    print(dataset)
+
+
+def test_fill_dataset_with_periods():
+    body = cpt.FloatingBody(mesh=cpt.mesh_horizontal_cylinder(radius=1, center=(0, 0, -2)),
+                            dofs=cpt.rigid_body_dofs(rotation_center=(0, 0, -2)))
+    T_range = np.linspace(1.0, 3.0, 3)
+    test_matrix = xr.Dataset(coords={'period': T_range, 'wave_direction': [0, np.pi/2], 'radiating_dof': ['Heave']})
+    dataset = solver.fill_dataset(test_matrix, [body])
+    np.testing.assert_allclose(sorted(dataset.coords['period']), sorted(T_range))