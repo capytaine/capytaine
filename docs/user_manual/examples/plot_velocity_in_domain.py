a917759fb35527d18e364f659669f272b9380316
Matthieu Ancellin <matthieu.ancellin@eurobios.com>
2023-03-24 12:34:12 +0100

    Clean up example and tests.

--- docs/user_manual/examples/plot_velocity_in_domain.py
+++ docs/user_manual/examples/plot_velocity_in_domain.py
@@ -1,44 +1,40 @@
 import numpy as np
 import capytaine as cpt
 
-# mesh = cpt.mesh_sphere(radius=1, center=(0, 0, 0), resolution=(10, 10)).immersed_part()
-mesh = cpt.mesh_horizontal_cylinder(radius=1.0, length=1.0, center=(0, 0, 0), resolution=(5, 20, 5)).immersed_part()
+# Solve the potential flow problems
+radius = 1.0
+mesh = cpt.mesh_horizontal_cylinder(radius=radius, length=1.0, center=(0, 0, 0), resolution=(5, 20, 5)).immersed_part()
 body = cpt.FloatingBody(mesh=mesh, dofs=cpt.rigid_body_dofs())
-velocities = {}
 solver = cpt.BEMSolver()
-for dof in body.dofs:
-    pb = cpt.RadiationProblem(body=body, omega=1.0, radiating_dof=dof)
-    res = solver.solve(pb)
-    velocities[dof] = solver.get_velocity_on_mesh(res, mesh)
+results = solver.solve_all([cpt.RadiationProblem(body=body, omega=1.0, radiating_dof=dof) for dof in body.dofs])
 
+# Define a set of points in the fluid on which the fluid velocities will be computed
 Y, Z = np.meshgrid(np.linspace(-3.0, 3.0, 40), np.linspace(-3.0, -0.05, 20))
 points = np.vstack([np.zeros(Y.size), Y.ravel(), Z.ravel()]).T
-points = np.array([[x, y, z] for (x, y, z) in points if y**2 + z**2 > 1])
+# Filter out points that are inside the floating body
+points = np.array([[x, y, z] for (x, y, z) in points if y**2 + z**2 > radius**2])
+
+# Compute the velocity field for each of the result object, that is each of the radiating dof
 velocities = {}
-for dof in body.dofs:
-    pb = cpt.RadiationProblem(body=body, omega=1.0, radiating_dof=dof)
-    res = solver.solve(pb)
-    velocities[dof] = solver.get_velocity_at_points(res, points)
+for res in results:
+    velocities[res.radiating_dof] = solver.get_velocity_at_points(res, points)
 
 import matplotlib.pyplot as plt
 fig, axs = plt.subplots(1, 2, figsize=(12, 4))
 for ax, dof in zip(axs.ravel(), ["Sway", "Heave"]):
+    # Display the arrows of the velocity
     y_velocities = -velocities[dof][:, 1].real
     z_velocities = -velocities[dof][:, 2].real
     ax.quiver(points[:, 1], points[:, 2], y_velocities, z_velocities, angles="xy")
     ax.axis('equal')
+
+    # Display the boudary of the floating body
     theta = np.linspace(-np.pi, 0.0, 100)
     ax.plot(np.cos(theta), np.sin(theta), color="red")
     ax.set_title(dof)
     ax.set_xlabel("y")
     ax.set_ylabel("z")
+
 plt.show()
 
-
-# import matplotlib.pyplot as plt
-# fig, axs = plt.subplots(2, 3, subplot_kw=dict(projection="3d"))
-# for ax, dof in zip(axs.ravel(), velocities):
-#     ax.quiver(*zip(*mesh.faces_centers), *zip(*velocities[dof]), length=0.5)
-#     ax.set_title(dof)
-# plt.show()
 
--- pytest/test_bem_velocity_reconstruction.py
+++ pytest/test_bem_velocity_reconstruction.py
@@ -2,40 +2,61 @@ import pytest
 import numpy as np
 import capytaine as cpt
 
-def test_gradient_G():
+
+def test_gradiant_G_shape():
     mesh = cpt.mesh_sphere(radius=1, center=(0, 0, 0), resolution=(10, 10)).immersed_part()
-    _, gradG_1 = cpt.Delhommeau().evaluate(mesh.faces_centers[[12], :], mesh, 0.0, -np.infty, 1.0, early_dot_product=False)
-    _, gradG_2 = cpt.Delhommeau().evaluate(mesh.copy(), mesh, 0.0, -np.infty, 1.0, early_dot_product=False)
-    _, gradG_3 = cpt.Delhommeau().evaluate(mesh, mesh, 0.0, -np.infty, 1.0, early_dot_product=False)
-    np.testing.assert_allclose(gradG_1, gradG_2[[12], :])
-    for i in range(mesh.nb_faces):
-        if i != 12:
-            np.testing.assert_allclose(gradG_1[:, i], gradG_3[[12], i])
-    np.testing.assert_allclose(gradG_1[0, 12], gradG_3[12, 12] - 0.5*mesh.faces_normals[12, :])
+    S, gradG_1 = cpt.Delhommeau().evaluate(mesh, mesh, 0.0, -np.infty, 1.0, early_dot_product=False)
+    assert gradG_1.shape == (mesh.nb_faces, mesh.nb_faces, 3)
 
 
-def test_a_posteriori_scalar_product():
+def test_gradient_G_a_posteriori_scalar_product():
     mesh = cpt.mesh_sphere(resolution=(4, 4)).immersed_part()
     S, gradG = cpt.Delhommeau().evaluate(mesh, mesh, 0.0, -np.infty, 1.0, early_dot_product=False)
     S, K = cpt.Delhommeau().evaluate(mesh, mesh, 0.0, -np.infty, 1.0, early_dot_product=True)
+
+    # Scalar product with the faces normal vectors
     K_ = np.zeros_like(S)
     for i in range(mesh.nb_faces):
         for j in range(mesh.nb_faces):
             K_[i, j] = gradG[i, j, :] @ mesh.faces_normals[i, :]
+
+    # Shorter implementiation of the exact same scalar products
     K__ = np.einsum('...jk,...k->...j', gradG, mesh.faces_normals)
+
     np.testing.assert_allclose(K, K_)
     np.testing.assert_allclose(K, K__)
 
 
-def test_manual_reconstruction_of_normal_velocity():
+def test_gradient_G_with_collocation_points():
+    mesh = cpt.mesh_sphere(radius=1, center=(0, 0, 0), resolution=(10, 10)).immersed_part()
+    _, gradG_1 = cpt.Delhommeau().evaluate(mesh.faces_centers, mesh, 0.0, -np.infty, 1.0, early_dot_product=False)
+    _, gradG_2 = cpt.Delhommeau().evaluate(mesh.copy(), mesh, 0.0, -np.infty, 1.0, early_dot_product=False)
+    np.testing.assert_allclose(gradG_1, gradG_2)
+
+
+def test_gradient_G_diagonal_term():
+    mesh = cpt.mesh_sphere(radius=1, center=(0, 0, 0), resolution=(10, 10)).immersed_part()
+    # Passing two different Python objects
+    _, gradG_1 = cpt.Delhommeau().evaluate(mesh.copy(), mesh, 0.0, -np.infty, 1.0, early_dot_product=False)
+    # Passing the same Python object
+    _, gradG_2 = cpt.Delhommeau().evaluate(mesh, mesh, 0.0, -np.infty, 1.0, early_dot_product=False)
+
+    diag_normal = np.zeros_like(gradG_2)
+    for i in range(mesh.nb_faces):
+        diag_normal[i, i, :] = mesh.faces_normals[i, :]
+
+    np.testing.assert_allclose(gradG_1, gradG_2 - 0.5*diag_normal)
+
+
+def test_reconstruction_of_given_boundary_condition():
     mesh = cpt.mesh_sphere(resolution=(4, 4)).immersed_part()
-    S, gradG = cpt.Delhommeau().evaluate(mesh, mesh, 0.0, -np.infty, 1.0, early_dot_product=False)
-    S, K = cpt.Delhommeau().evaluate(mesh, mesh, 0.0, -np.infty, 1.0, early_dot_product=True)
-    neumann_bc = np.random.rand(mesh.nb_faces)
-    sources = np.linalg.solve(K, neumann_bc)
-    velocities = np.einsum('ijk,j->ik', gradG, sources)
+    body = cpt.FloatingBody(mesh=mesh, dofs={"random_dof": np.random.rand(mesh.nb_faces, 3)})
+    pb = cpt.RadiationProblem(body=body, omega=1.0, radiating_dof="random_dof")
+    solver = cpt.BEMSolver()
+    res = solver.solve(pb)
+    velocities = solver.get_velocity_at_points(res, mesh)
     normal_velocities = np.einsum('...k,...k->...', velocities, mesh.faces_normals)
-    np.testing.assert_allclose(normal_velocities, neumann_bc)
+    np.testing.assert_allclose(normal_velocities, pb.boundary_condition)
 
 
 def test_a_posteriori_scalar_product_direct_method():
