f50e342a8b2a02b4f6535d4588cf867098852831
Matthieu Ancellin <31126826+mancellin@users.noreply.github.com>
2023-04-25 16:09:56 +0200

    Remove outdated conda and paper directories. (#333)

--- conda/bld.bat
+++
@@ -1,8 +0,0 @@
-echo [build]           >> %MINICONDA%\Lib\distutils\distutils.cfg
-echo compiler=mingw32  >> %MINICONDA%\Lib\distutils\distutils.cfg
-
-"%PYTHON%" setup.py build_ext
-"%PYTHON%" setup.py build_ext --inplace
-"%PYTHON%" setup.py install
-if errorlevel 1 exit 1
-
