d3898e6d899ec47817d6e1953e44e89aac10ef4f
Matthieu Ancellin <matthieu.ancellin@eurobios.com>
2023-10-03 12:33:34 +0200

    Fix locked env test.

--- noxfile.py
+++ noxfile.py
@@ -8,18 +8,23 @@ import nox
 nox.options.envdir = os.path.join(tempfile.gettempdir(), "nox-capytaine")
 
 os.environ.update({"PDM_IGNORE_SAVED_PYTHON": "1"})
+os.environ.update({"PDM_USE_VENV": "1"})
+
+NOXFILE_DIR = os.path.dirname(__file__)
+
+def run_tests(session):
+    with session.chdir(session.create_tmp()):
+        session.run("python", "-m", "pytest", os.path.join(NOXFILE_DIR, "pytest"))
+        session.run('python', '-c', '"import capytaine; print(capytaine.__version__)"')
+        session.run('capytaine', '--help')
+        for example_file in ["custom_dofs.py", "haskind.py", "multibody.py"]:
+            session.run('python', os.path.join(NOXFILE_DIR, "examples", example_file))
 
 
 @nox.session
 def build_and_test_on_latest_env(session):
-    session.install(".[ci]")
-    noxfile_dir = os.path.dirname(__file__)
-    with session.chdir(session.create_tmp()):
-        session.run("pytest", os.path.join(noxfile_dir, "pytest"))
-        session.run('python', '-c', '"import capytaine; print(capytaine.__version__)"')
-        session.run('capytaine', '--help')
-        for example_file in ["custom_dofs.py", "haskind.py", "multibody.py"]:
-            session.run('python', os.path.join(noxfile_dir, "examples", example_file))
+    session.install(".[test]")
+    run_tests(session)
 
 
 @nox.session
@@ -27,12 +32,9 @@ def build_and_test_on_latest_env(session):
 def build_and_test_on_locked_env(session, env_file):
     session.install("pdm")
     session.run_always('pdm', 'sync', '--no-self', '-L', f"pytest/envs/{env_file}.lock")
+    # Lock file was created with
+    # pdm lock -d -G build -G test -L pytest/envs/2023-10-02.lock
     session.install("--no-build-isolation", "--no-deps", "-e", ".")
-    noxfile_dir = os.path.dirname(__file__)
-    with session.chdir(session.create_tmp()):
-        session.run("pytest", os.path.join(noxfile_dir, "pytest"))
-        session.run('python', '-c', '"import capytaine; print(capytaine.__version__)"')
-        session.run('capytaine', '--help')
-        for example_file in ["custom_dofs.py", "haskind.py", "multibody.py"]:
-            session.run('python', os.path.join(noxfile_dir, "examples", example_file))
-
+    # Editable install using pip and not pdm because pdm's editable install
+    # does not seems to work with meson-python.
+    run_tests(session)
