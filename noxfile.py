820c5a4041c8607c36aa2242ff2e6a52dc7c0b0e
Matthieu Ancellin <matthieu.ancellin@eurobios.com>
2023-10-10 17:06:08 +0200

    Run more examples in CI and add some optional dependencies to pyproject.toml.

--- noxfile.py
+++ noxfile.py
@@ -12,18 +12,20 @@ os.environ.update({"PDM_USE_VENV": "1"})
 
 NOXFILE_DIR = os.path.dirname(__file__)
 
+EXAMPLE_FILES = ["compare_Green_functions.py", "convergence_study.py", "custom_dofs.py", "custom_Green_function.py", "finite_depth_cylinder.py", "free_surface_elevation.py", "haskind.py", "kochin.py", "multibody.py", "plot_influence_matrix.py", "plot_velocity_in_domain.py", "radiation_cylinder.py"]
+
 def run_tests(session):
     with session.chdir(session.create_tmp()):
         session.run("python", "-m", "pytest", os.path.join(NOXFILE_DIR, "pytest"))
         session.run('python', '-c', '"import capytaine; print(capytaine.__version__)"')
         session.run('capytaine', '--help')
-        for example_file in ["custom_dofs.py", "haskind.py", "multibody.py"]:
-            session.run('python', os.path.join(NOXFILE_DIR, "examples", example_file))
+        for example_file in EXAMPLE_FILES:
+            session.run('python', os.path.join(NOXFILE_DIR, "examples", example_file), env={'MPLBACKEND': 'pdf'})
 
 
 @nox.session
 def build_and_test_on_latest_env(session):
-    session.install(".[test]")
+    session.install(".[test,optional]")
     run_tests(session)
 
 
@@ -37,4 +39,5 @@ def build_and_test_on_locked_env(session, env_file):
     session.install("--no-build-isolation", "--no-deps", "-e", ".")
     # Editable install using pip and not pdm because pdm's editable install
     # does not seems to work with meson-python.
+    session.install("matplotlib")  # Need to be installed in the lock file in the future.
     run_tests(session)
