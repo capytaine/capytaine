62c56ae0d639109d94286e7ac35e0393ceedeed3
Matthieu Ancellin <matthieu.ancellin@eurobios.com>
2023-10-31 11:45:16 +0100

    Rephrase resolution warnings.

--- capytaine/bem/problems_and_results.py
+++ capytaine/bem/problems_and_results.py
@@ -165,13 +165,6 @@ class LinearPotentialFlowProblem:
                     "or use body.keep_immersed_part() to clip the mesh."
                 )
 
-            if self.wavelength < self.body.minimal_computable_wavelength:
-                LOG.warning(f"Mesh resolution for {self}:\n"
-                        f"The resolution of the mesh '{self.body.mesh.name}' of the body '{self.body.name}' "
-                        f"might be insufficient for the wavelength λ={self.wavelength:.2e}.\n"
-                        f"This warning appears because the largest panel of this mesh has radius {self.body.mesh.faces_radiuses.max():.2e} > λ/8."
-                        )
-
         if self.boundary_condition is not None:
             if len(self.boundary_condition.shape) != 1:
                 raise ValueError("Expected a 1-dimensional array as boundary_condition")
--- capytaine/ui/rich.py
+++ capytaine/ui/rich.py
@@ -2,4 +2,4 @@ import logging
 from rich.logging import RichHandler
 
 def set_logging(level="INFO"):
-    logging.basicConfig(level=level, format="%(message)s", handlers=[RichHandler(level=level, show_path=False)], force=True)
+    logging.basicConfig(level=level, format="%(message)s", handlers=[RichHandler(level=level, log_time_format="[%X]", show_path=False)], force=True)
