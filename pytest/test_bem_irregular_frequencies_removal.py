50e072ba4301b5c3a5f9d852d3e31fdba962aa14
Matthieu Ancellin <matthieu.ancellin@eurobios.com>
2023-11-27 12:04:03 +0100

    Extend test for finite depth.

--- pytest/test_bem_irregular_frequencies_removal.py
+++ pytest/test_bem_irregular_frequencies_removal.py
@@ -1,12 +1,13 @@
-# import pytest
+import pytest
 
 import numpy as np
 import capytaine as cpt
 
 
-def test_panel_on_free_surface():
+@pytest.mark.parametrize("water_depth", [np.infty, 10.0])
+def test_panel_on_free_surface(water_depth):
     mesh = cpt.mesh_parallelepiped(center=(0.0, 0.0, -0.5))
     body = cpt.FloatingBody(mesh, cpt.rigid_body_dofs())
     body.mesh.compute_quadrature("Gauss-Legendre 2")
-    pb = cpt.RadiationProblem(body=body, wavelength=1.0, radiating_dof="Heave")
+    pb = cpt.RadiationProblem(body=body, wavelength=1.0, water_depth=water_depth, radiating_dof="Heave")
     res = cpt.BEMSolver().solve(pb)
