bee2afd2b50c22f7efe9ec4fd4332d420e4773e3
Matthieu Ancellin <31126826+mancellin@users.noreply.github.com>
2024-03-19 11:36:42 +0100

    Save tabulation on disk at initialization. (#454)

---
+++ capytaine/tools/cache_on_disk.py
@@ -0,0 +1,24 @@
+"""
+Adapted from https://github.com/platformdirs/platformdirs (MIT Licensed)
+"""
+import os
+import sys
+from pathlib import Path
+
+from capytaine import __version__
+
+
+def cache_directory():
+    if sys.platform == "win32":  # Windows
+        path = os.path.normpath(os.environ.get("LOCALAPPDATA"))
+        path = os.path.join(path, "capytaine", "Cache", __version__)
+    elif sys.platform == "darwin":  # MacOS
+        path = os.path.expanduser("~/Library/Caches")
+        path = os.path.join(path, "capytaine", __version__)
+    else:
+        path = os.environ.get("XDG_CACHE_HOME", "")
+        if path.strip() == "":
+            path = os.path.expanduser("~/.cache")
+        path = os.path.join(path, "capytaine", __version__)
+    Path(path).mkdir(parents=True, exist_ok=True)
+    return path
