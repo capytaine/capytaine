854eb395c773a98558e7d16b6fe44daeddf03728
Matthieu Ancellin <31126826+mancellin@users.noreply.github.com>
2024-04-30 15:20:16 +0200

    Add section about outputs.

--- docs/user_manual/examples/pressure_on_hull.py
+++ docs/user_manual/examples/pressure_on_hull.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 mesh = cpt.mesh_sphere(faces_max_radius=0.1).immersed_part()
 body = cpt.FloatingBody(mesh=mesh, dofs=cpt.rigid_body_dofs(rotation_center=(0, 0, 0)))
 
-pb = cpt.RadiationProblem(wavelength=0.1, body=body, radiating_dof="Surge", water_depth=10.0)
+pb = cpt.RadiationProblem(wavelength=1.0, body=body, radiating_dof="Surge", water_depth=10.0)
 solver = cpt.BEMSolver()
 res = solver.solve(pb, keep_details=True)
 
