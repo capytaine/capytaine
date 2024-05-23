6855a4a7e74a866656c2ea266301148ff39316da
Matthieu Ancellin <31126826+mancellin@users.noreply.github.com>
2024-05-23 15:40:33 +0200

    Edit and move some tests around.

--- pytest/test_bem_fortran_core.py
+++ pytest/test_bem_fortran_core.py
@@ -1,9 +1,9 @@
 """Tests for the computation of the Green function using Fortran routines."""
 
 import pytest
+
 import numpy as np
 import capytaine as cpt
-from pytest import approx
 
 # def test_exponential_integral(x, y):
 #     # Compare with Scipy implementation
@@ -20,7 +20,9 @@ from pytest import approx
 #     if y != 0.0:
 #         assert np.isclose(E1(np.conjugate(z)), np.conjugate(E1(z)), rtol=1e-3)
 
-def test_infinite_depth_gf():
+
+def test_infinite_depth_gf_singularities_variants():
+    """Test how the values of the variants of the infinite depth Green function are related."""
     from scipy.special import j0
     gf = cpt.Delhommeau()
     def wave_part(*args):
@@ -42,6 +44,8 @@ def test_infinite_depth_gf():
 
 @pytest.mark.parametrize("gf_singularities", ["low_freq", "high_freq"])
 def test_infinite_depth_gf_finite_differences(gf_singularities):
+    """Compare the gradient of the Green function as returned by the code with the finite difference gradient"""
+    # TODO: same in finite depth
     gf = cpt.Delhommeau(gf_singularities=gf_singularities)
     def wave_part(*args):
         return gf.fortran_core.green_wave.wave_part_infinite_depth(
@@ -71,8 +75,8 @@ def test_infinite_depth_gf_finite_differences(gf_singularities):
 gfs = [
         cpt.Delhommeau(tabulation_nr=328, tabulation_nz=46, tabulation_nb_integration_points=251, tabulation_grid_shape="legacy"),
         cpt.XieDelhommeau(tabulation_nr=328, tabulation_nz=46, tabulation_nb_integration_points=251, tabulation_grid_shape="legacy"),
+        # TODO: add more relevant Green functions
         ]
-
 
 @pytest.mark.parametrize("gf", gfs)
 def test_symmetry_of_the_green_function_infinite_depth(gf):
@@ -91,9 +95,9 @@ def test_symmetry_of_the_green_function_infinite_depth(gf):
         gf.tabulated_r_range, gf.tabulated_z_range,
         gf.tabulated_integrals, gf.gf_singularities_index
     )
-    assert g1 == approx(g2)
-    assert dg1[0:2] == approx(-dg2[0:2])
-    assert dg1[2] == approx(dg2[2])
+    assert g1 == pytest.approx(g2)
+    assert dg1[0:2] == pytest.approx(-dg2[0:2])
+    assert dg1[2] == pytest.approx(dg2[2])
 
 
 @pytest.mark.parametrize("gf", gfs)
@@ -116,9 +120,9 @@ def test_symmetry_of_the_green_function_finite_depth_no_prony(gf):
         gf.tabulated_integrals,
         np.zeros(1), np.zeros(1), 1
     )
-    assert g1 == approx(g2)
-    assert dg1_sym == approx(dg2_sym)
-    assert dg1_antisym == approx(-dg2_antisym)
+    assert g1 == pytest.approx(g2)
+    assert dg1_sym == pytest.approx(dg2_sym)
+    assert dg1_antisym == pytest.approx(-dg2_antisym)
 
 
 @pytest.mark.parametrize("gf", gfs)
@@ -142,11 +146,72 @@ def test_symmetry_of_the_green_function_finite_depth(gf):
         gf.tabulated_integrals,
         ambda, a, 31
     )
-    assert g1 == approx(g2)
-    assert dg1_sym == approx(dg2_sym)
-    assert dg1_antisym == approx(-dg2_antisym)
+    assert g1 == pytest.approx(g2)
+    assert dg1_sym == pytest.approx(dg2_sym)
+    assert dg1_antisym == pytest.approx(-dg2_antisym)
 
 
 def test_floating_point_precision():
     assert cpt.Delhommeau(floating_point_precision="float64").tabulated_integrals.dtype == np.float64
     assert cpt.Delhommeau(floating_point_precision="float32").tabulated_integrals.dtype == np.float32
+
+
+def test_no_tabulation():
+    mesh = cpt.mesh_sphere().immersed_part()
+    tabed_gf = cpt.Delhommeau()
+    untabed_gf = cpt.Delhommeau(tabulation_nr=0, tabulation_nz=0)
+    assert np.allclose(untabed_gf.evaluate(mesh, mesh)[0], tabed_gf.evaluate(mesh, mesh)[0], atol=1e-2)
+
+
+# def test_panels_near_free_surface():
+#     area = 4.0
+#     def gf_at_depth(d):
+#         mesh = cpt.mesh_rectangle(size=(np.sqrt(area), np.sqrt(area)), resolution=(1, 1), center=(0, 0, d))
+#         x = np.array([[0, 0, 0]])
+#         S, _ = cpt.Delhommeau().evaluate(x, mesh, free_surface=0, water_depth=np.infty, wavenumber=3.0)
+#         return S
+#     gf_at_depth(0.0)
+#     gf_at_depth(1e-9)
+#     gf_at_depth(1e-5)
+#     gf_at_depth(1e-2)
+
+
+def test_exact_integration_of_rankine_terms():
+    gf = cpt.Delhommeau()
+    center = np.array([0.0, 0.0, 0.0])
+    area = 1.0
+
+    mesh = cpt.mesh_rectangle(size=(np.sqrt(area), np.sqrt(area)), center=center, resolution=(1, 1))
+    rankine_g_once = gf.evaluate(center.reshape(1, -1), mesh, wavenumber=0.0)[0]
+    mesh = cpt.mesh_rectangle(size=(np.sqrt(area), np.sqrt(area)), center=center, resolution=(11, 11))
+    # Use odd number of panels to avoid having the center on a corner of a panel, which is not defined for the strong singularity of the derivative.
+    rankine_g_parts = gf.evaluate(center.reshape(1, -1), mesh, wavenumber=0.0)[0]
+    assert rankine_g_once == pytest.approx(rankine_g_parts.sum(), abs=1e-2)
+
+
+def test_exact_integration_with_nb_integration_points():
+    # Test that the tabulation_nb_integration_points is actually taken into account.
+    mesh = cpt.mesh_rectangle(center=(0, 0, -1), resolution=(1, 1))
+    point = np.array([[100, 0, -1]])
+    gf = cpt.Delhommeau(tabulation_nr=0, tabulation_nb_integration_points=11)
+    val1 = gf.evaluate(point, mesh)
+    gf = cpt.Delhommeau(tabulation_nr=0, tabulation_nb_integration_points=101)
+    val2 = gf.evaluate(point, mesh)
+    assert np.abs(val1[0] - val2[0]) > 1e-1
+
+
+def test_low_freq_with_rankine_part_singularities():
+    # TODO: also for not adjoint double layer
+    gf1 = cpt.Delhommeau(gf_singularities="low_freq")
+    gf2 = cpt.Delhommeau(gf_singularities="low_freq_with_rankine_part")
+    point = np.array([[0.0, 0.0, -1.0]])
+    area = 1.0
+    wavenumber = 1.0
+    mesh = cpt.mesh_rectangle(size=(np.sqrt(area), np.sqrt(area)), center=(0, 0, -1), resolution=(1, 1))
+    S1, K1 = gf1.evaluate(point, mesh, wavenumber=wavenumber, early_dot_product=False)
+    S2, K2 = gf2.evaluate(point, mesh, wavenumber=wavenumber, early_dot_product=False)
+    assert S1 == S2
+    assert np.allclose(K1[:, :, :2], K2[:, :, :2], atol=1e-12)
+    assert np.allclose(K1[:, :, 2].imag, K2[:, :, 2].imag, atol=1e-12)
+    assert not np.allclose(K1[0, 0, 2].real, K2[0, 0, 2].real, atol=1e-12)
+    assert np.allclose(K1[0, 0, 2].real, K2[0, 0, 2].real, atol=1e-1)
--- pytest/test_bem_green_functions.py
+++ pytest/test_bem_green_functions.py
@@ -1,71 +1,9 @@
-"""Tests for the cpt.Delhommeau() class"""
+"""Tests for the matrices returned by cpt.Delhommeau() class"""
 
 import pytest
 
 import numpy as np
 import capytaine as cpt
-
-
-def test_no_tabulation():
-    mesh = cpt.mesh_sphere().immersed_part()
-    tabed_gf = cpt.Delhommeau()
-    untabed_gf = cpt.Delhommeau(tabulation_nr=0, tabulation_nz=0)
-    assert np.allclose(untabed_gf.evaluate(mesh, mesh)[0], tabed_gf.evaluate(mesh, mesh)[0], atol=1e-2)
-
-
-# def test_panels_near_free_surface():
-#     area = 4.0
-#     def gf_at_depth(d):
-#         mesh = cpt.mesh_rectangle(size=(np.sqrt(area), np.sqrt(area)), resolution=(1, 1), center=(0, 0, d))
-#         x = np.array([[0, 0, 0]])
-#         S, _ = cpt.Delhommeau().evaluate(x, mesh, free_surface=0, water_depth=np.infty, wavenumber=3.0)
-#         return S
-#     gf_at_depth(0.0)
-#     gf_at_depth(1e-9)
-#     gf_at_depth(1e-5)
-#     gf_at_depth(1e-2)
-
-
-def test_exact_integration_of_rankine_terms():
-    gf = cpt.Delhommeau()
-    center = np.array([0.0, 0.0, 0.0])
-    area = 1.0
-
-    mesh = cpt.mesh_rectangle(size=(np.sqrt(area), np.sqrt(area)), center=center, resolution=(1, 1))
-    rankine_g_once = gf.evaluate(center.reshape(1, -1), mesh, wavenumber=0.0)[0]
-    mesh = cpt.mesh_rectangle(size=(np.sqrt(area), np.sqrt(area)), center=center, resolution=(11, 11))
-    # Use odd number of panels to avoid having the center on a corner of a panel, which is not defined for the strong singularity of the derivative.
-    rankine_g_parts = gf.evaluate(center.reshape(1, -1), mesh, wavenumber=0.0)[0]
-    assert rankine_g_once == pytest.approx(rankine_g_parts.sum(), abs=1e-2)
-
-
-def test_exact_integration_with_nb_integration_points():
-    # Test that the tabulation_nb_integration_points is actually taken into account.
-    mesh = cpt.mesh_rectangle(center=(0, 0, -1), resolution=(1, 1))
-    point = np.array([[100, 0, -1]])
-    gf = cpt.Delhommeau(tabulation_nr=0, tabulation_nb_integration_points=11)
-    val1 = gf.evaluate(point, mesh)
-    gf = cpt.Delhommeau(tabulation_nr=0, tabulation_nb_integration_points=101)
-    val2 = gf.evaluate(point, mesh)
-    assert np.abs(val1[0] - val2[0]) > 1e-1
-
-
-def test_low_freq_with_rankine_part_singularities():
-    import numpy as np
-    import capytaine as cpt
-    gf1 = cpt.Delhommeau(gf_singularities="low_freq")
-    gf2 = cpt.Delhommeau(gf_singularities="low_freq_with_rankine_part")
-    point = np.array([[0.0, 0.0, -1.0]])
-    area = 1.0
-    wavenumber = 1.0
-    mesh = cpt.mesh_rectangle(size=(np.sqrt(area), np.sqrt(area)), center=(0, 0, -1), resolution=(1, 1))
-    S1, K1 = gf1.evaluate(point, mesh, wavenumber=wavenumber, early_dot_product=False)
-    S2, K2 = gf2.evaluate(point, mesh, wavenumber=wavenumber, early_dot_product=False)
-    assert S1 == S2
-    assert np.allclose(K1[:, :, :2], K2[:, :, :2], atol=1e-12)
-    assert np.allclose(K1[:, :, 2].imag, K2[:, :, 2].imag, atol=1e-12)
-    assert not np.allclose(K1[0, 0, 2].real, K2[0, 0, 2].real, atol=1e-12)
-    assert np.allclose(K1[0, 0, 2].real, K2[0, 0, 2].real, atol=1e-1)
 
 
 @pytest.mark.parametrize("adjoint_double_layer", [True, False])
@@ -124,10 +62,10 @@ def test_gradient_G_diagonal_term(adjoint_double_layer):
     mesh = cpt.mesh_sphere(radius=1, center=(0, 0, 0), resolution=(10, 10)).immersed_part()
     # Passing two different Python objects
     _, gradG_1 = cpt.Delhommeau().evaluate(mesh.copy(), mesh, 0.0, np.inf, 1.0,
-					early_dot_product=False, adjoint_double_layer=adjoint_double_layer)
+                                           early_dot_product=False, adjoint_double_layer=adjoint_double_layer)
     # Passing the same Python object
     _, gradG_2 = cpt.Delhommeau().evaluate(mesh, mesh, 0.0, np.inf, 1.0,
-					early_dot_product=False, adjoint_double_layer=adjoint_double_layer)
+                                           early_dot_product=False, adjoint_double_layer=adjoint_double_layer)
 
     diag_normal = np.zeros_like(gradG_2)
     for i in range(mesh.nb_faces):
