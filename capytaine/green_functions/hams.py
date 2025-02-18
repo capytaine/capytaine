3ff339b1355b9ff1a885f2725518acbe6e8c4b58
Matthieu Ancellin <31126826+mancellin@users.noreply.github.com>
2025-02-18 14:41:49 +0100

    refactor: move common code to AbstractGreenFunction (#618)

--- capytaine/green_functions/hams.py
+++ capytaine/green_functions/hams.py
@@ -1,8 +1,6 @@
 from importlib import import_module
 import numpy as np
 
-from capytaine.meshes.meshes import Mesh
-from capytaine.meshes.collections import CollectionOfMeshes
 from capytaine.green_functions.abstract_green_function import AbstractGreenFunction
 
 
@@ -46,33 +44,9 @@ class LiangWuNoblesseGF(AbstractGreenFunction):
             else:
                 coeffs = np.array((1.0, 1.0, 1.0))
 
-        if isinstance(mesh1, Mesh) or isinstance(mesh1, CollectionOfMeshes):
-            collocation_points = mesh1.faces_centers
-            nb_collocation_points = mesh1.nb_faces
-            if not adjoint_double_layer: # Computing the D matrix
-                early_dot_product_normals = mesh2.faces_normals
-            else: # Computing the K matrix
-                early_dot_product_normals = mesh1.faces_normals
+        collocation_points, early_dot_product_normals = self._get_colocation_points_and_normals(mesh1, mesh2, adjoint_double_layer)
 
-        elif isinstance(mesh1, np.ndarray) and mesh1.ndim == 2 and mesh1.shape[1] == 3:
-            # This is used when computing potential or velocity at given points in postprocessing
-            collocation_points = mesh1
-            nb_collocation_points = mesh1.shape[0]
-            if not adjoint_double_layer: # Computing the D matrix
-                early_dot_product_normals = mesh2.faces_normals
-            else: # Computing the K matrix
-                early_dot_product_normals = np.zeros((nb_collocation_points, 3))
-                # Dummy argument since this method is meant to be used either
-                # - to compute potential, then only S is needed and early_dot_product_normals is irrelevant,
-                # - to compute velocity, then the adjoint full gradient is needed and early_dot_product is False and this value is unused.
-                # TODO: add an only_S argument and return an error here if (early_dot_product and not only_S)
-
-        else:
-            raise ValueError(f"Unrecognized first input for {self.__class__.__name__}.evaluate:\n{mesh1}")
-
-        dtype = "complex128"
-        S = np.empty((nb_collocation_points, mesh2.nb_faces), order="F", dtype=dtype)
-        K = np.empty((nb_collocation_points, mesh2.nb_faces, 1 if early_dot_product else 3), order="F", dtype=dtype)
+        S, K = self._init_matrices((collocation_points.shape[0], mesh2.nb_faces), "complex128", early_dot_product=early_dot_product)
 
         self.fortran_core.matrices.build_matrices(
             collocation_points,  early_dot_product_normals,
@@ -93,7 +67,7 @@ class LiangWuNoblesseGF(AbstractGreenFunction):
             raise RuntimeError("Green function returned a NaN in the interaction matrix.\n"
                     "It could be due to overlapping panels.")
 
-        if early_dot_product: K = K.reshape((nb_collocation_points, mesh2.nb_faces))
+        if early_dot_product: K = K.reshape((collocation_points.shape[0], mesh2.nb_faces))
 
         return S, K
 
