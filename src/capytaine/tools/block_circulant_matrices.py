df28c274eb2e6e5d46029fb49d5ac06b9f6ba950
Matthieu Ancellin <31126826+mancellin@users.noreply.github.com>
2025-09-12 15:55:49 +0200

    Fix tests.

--- src/capytaine/tools/block_circulant_matrices.py
+++ src/capytaine/tools/block_circulant_matrices.py
@@ -65,14 +65,15 @@ class BlockCirculantMatrix:
     def block_diagonalize(self) -> "BlockDiagonalMatrix":
         if all(isinstance(b, BlockCirculantMatrix) for b in self.blocks) and self.nb_blocks == 2:
             a, b = self.blocks
-            return BlockDiagonalMatrix([(a + b).block_diagonalize(), (a - b).block_diagonalize()])
+            return BlockDiagonalMatrix([a + b, a - b])
         elif all(isinstance(b, np.ndarray) for b in self.blocks):
             return BlockDiagonalMatrix(np.fft.fft(np.array(self.blocks), axis=0))
         else:
             raise NotImplementedError()
 
     def solve(self, b: np.ndarray) -> np.ndarray:
-        LOG.debug("Called solve on %s of shape %s with rhs: %s", self.__class__.__name__, self.shape, b)
+        LOG.debug("Called solve on %s of shape %s",
+                  self.__class__.__name__, self.shape)
         n = self.nb_blocks
         b_fft = np.fft.fft(b.reshape((n, b.shape[0]//n)), axis=0).reshape(b.shape)
         res_fft = self.block_diagonalize().solve(b_fft)
@@ -117,7 +118,8 @@ class BlockDiagonalMatrix:
             for i in range(self.nb_blocks)])
 
     def solve(self, b: np.ndarray) -> np.ndarray:
-        LOG.debug("Called solve on %s of shape %s with rhs: %s", self.__class__.__name__, self.shape, b)
+        LOG.debug("Called solve on %s of shape %s",
+                  self.__class__.__name__, self.shape)
         n = self.nb_blocks
         rhs = np.split(b, n)
         res = [np.linalg.solve(Ai, bi) if isinstance(Ai, np.ndarray) else Ai.solve(bi)
@@ -139,9 +141,14 @@ def lu_decompose(A: ArrayLike):
 
 class LUDecomposedMatrix:
     def __init__(self, A: np.ndarray):
+        LOG.debug("LU decomp of %s of shape %s",
+                  A.__class__.__name__, A.shape)
         self._lu_decomp = sl.lu_factor(A)
+        self.shape = A.shape
 
     def solve(self, b: np.ndarray) -> np.ndarray:
+        LOG.debug("Called solve on %s of shape %s",
+                  self.__class__.__name__, self.shape)
         return sl.lu_solve(self._lu_decomp, b)
 
 
@@ -149,20 +156,31 @@ class LUDecomposedBlockDiagonalMatrix:
     """LU decomposition of a BlockDiagonalMatrix,
     stored as the LU decomposition of each block."""
     def __init__(self, bdm: BlockDiagonalMatrix):
+        LOG.debug("LU decomp of %s of shape %s",
+                  bdm.__class__.__name__, bdm.shape)
         self._lu_decomp = [lu_decompose(bl) for bl in bdm.blocks]
+        self.shape = bdm.shape
+        self.nb_blocks = bdm.nb_blocks
 
     def solve(self, b: np.ndarray) -> np.ndarray:
-        rhs = np.split(b, len(self._lu_decomp))
+        LOG.debug("Called solve on %s of shape %s",
+                  self.__class__.__name__, self.shape)
+        rhs = np.split(b, self.nb_blocks)
         res = [Ai.solve(bi) for (Ai, bi) in zip(self._lu_decomp, rhs)]
         return np.hstack(res)
 
 
 class LUDecomposedBlockCirculantMatrix:
     def __init__(self, bcm: BlockCirculantMatrix):
+        LOG.debug("LU decomp of %s of shape %s",
+                  bcm.__class__.__name__, bcm.shape)
         self._lu_decomp = lu_decompose(bcm.block_diagonalize())
+        self.shape = bcm.shape
         self.nb_blocks = bcm.nb_blocks
 
     def solve(self, b: np.ndarray) -> np.ndarray:
+        LOG.debug("Called solve on %s of shape %s",
+                  self.__class__.__name__, self.shape)
         n = self.nb_blocks
         b_fft = np.fft.fft(b.reshape((n, b.shape[0]//n)), axis=0).reshape(b.shape)
         res_fft = self._lu_decomp.solve(b_fft)
