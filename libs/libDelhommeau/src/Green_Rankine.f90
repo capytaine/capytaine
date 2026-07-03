fb20545e21131305f3d39ef5d9fd18444983d1af
Matthieu Ancellin <31126826+mancellin@users.noreply.github.com>
2026-07-03 16:08:31 +0200

    Relisence to Apache license

--- libs/libDelhommeau/src/Green_Rankine.f90
+++ libs/libDelhommeau/src/Green_Rankine.f90
@@ -1,5 +1,16 @@
-! Copyright (C) 2017-2025 Matthieu Ancellin
-! See LICENSE file at <https://github.com/capytaine/libDelhommeau>
+! Copyright 2026 Capytaine developers
+!
+! Licensed under the Apache License, Version 2.0 (the "License");
+! you may not use this file except in compliance with the License.
+! You may obtain a copy of the License at
+!
+!     http://www.apache.org/licenses/LICENSE-2.0
+!
+! Unless required by applicable law or agreed to in writing, software
+! distributed under the License is distributed on an "AS IS" BASIS,
+! WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+! See the License for the specific language governing permissions and
+! limitations under the License.
 module Green_Rankine
 
   use floating_point_precision, only: pre
@@ -152,7 +163,7 @@ contains
       face_center, face_area,                    &
       derivative_with_respect_to_first_variable, &
       int_G, int_nabla_G)
-    ! Same as above, but always use the approximate aymptotic value.
+    ! Same as above, but always use the approximate asymptotic value.
 
     ! Inputs
     real(kind=pre), dimension(3), intent(in) :: M
